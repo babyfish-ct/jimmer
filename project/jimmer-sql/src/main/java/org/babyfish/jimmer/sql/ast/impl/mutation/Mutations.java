cacad34f09712298d3f1e4e3ecae0c893ec6c515
babyfish-ct <babyfish.ct@gmail.com>
2022-05-12 03:27:36 +0800

    Support update

--- project/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/ast/impl/mutation/Mutations.java
+++ project/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/ast/impl/mutation/Mutations.java
@@ -3,8 +3,10 @@ package org.babyfish.jimmer.sql.ast.impl.mutation;
 import org.babyfish.jimmer.meta.ImmutableType;
 import org.babyfish.jimmer.sql.SqlClient;
 import org.babyfish.jimmer.sql.ast.Executable;
+import org.babyfish.jimmer.sql.ast.mutation.MutableDelete;
 import org.babyfish.jimmer.sql.ast.mutation.MutableUpdate;
 import org.babyfish.jimmer.sql.ast.table.Table;
+import org.babyfish.jimmer.sql.ast.table.TableEx;
 
 import java.util.function.BiConsumer;
 
@@ -12,7 +14,7 @@ public class Mutations {
 
     private Mutations() {}
 
-    public static <T extends Table<?>> Executable<Integer> createUpdate(
+    public static <T extends TableEx<?>> Executable<Integer> createUpdate(
             SqlClient sqlClient,
             Class<T> tableType,
             BiConsumer<MutableUpdate, T> block
@@ -24,5 +26,20 @@ public class Mutations {
         block.accept(update, update.getTable());
         update.freeze();
         return update;
+    }
+
+    public static <T extends TableEx<?>> Executable<Integer> createDelete(
+            SqlClient sqlClient,
+            Class<T> tableType,
+            BiConsumer<MutableDelete, T> block
+    ) {
+        throw new UnsupportedOperationException();
+//        MutableDeleteImpl delete = new MutableDeleteImpl(
+//                sqlClient,
+//                ImmutableType.tryGet(tableType)
+//        );
+//        block.accept(delete, delete.getTable());
+//        delete.freeze();
+//        return delete;
     }
 }
