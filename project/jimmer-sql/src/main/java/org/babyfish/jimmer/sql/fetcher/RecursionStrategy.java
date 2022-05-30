10da0d2d39664426dece5b43d60fcd7da72eea2e
babyfish-ct <babyfish.ct@gmail.com>
2022-05-30 21:38:08 +0800

    0.0.18, RecursionStrategy can work on root node

--- project/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/fetcher/RecursionStrategy.java
+++ project/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/fetcher/RecursionStrategy.java
@@ -2,5 +2,33 @@ package org.babyfish.jimmer.sql.fetcher;
 
 public interface RecursionStrategy<E> {
 
-    boolean isFetchable(E entity, int depth);
+    boolean isRecursive(Args<E> args);
+
+    class Args<E> {
+
+        private E entity;
+
+        private int depth;
+
+        public Args(E entity, int depth) {
+            this.entity = entity;
+            this.depth = depth;
+        }
+
+        public E getEntity() {
+            return entity;
+        }
+
+        public int getDepth() {
+            return depth;
+        }
+
+        @Override
+        public String toString() {
+            return "Args{" +
+                    "entity=" + entity +
+                    ", depth=" + depth +
+                    '}';
+        }
+    }
 }
