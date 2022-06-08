c29c50adae0e23a88a6257e0d0c6699e20134c37
babyfish-ct <babyfish.ct@gmail.com>
2022-06-08 17:12:52 +0800

    Let StringExpression extends ComparableExpression<String>

--- project/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/ast/impl/StringExpressionImplementor.java
+++ project/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/ast/impl/StringExpressionImplementor.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-interface StringExpressionImplementor extends StringExpression, ExpressionImplementor<String> {
+interface StringExpressionImplementor extends StringExpression, ComparableExpressionImplementor<String> {
 
     @Override
     default Class<String> getType() {
