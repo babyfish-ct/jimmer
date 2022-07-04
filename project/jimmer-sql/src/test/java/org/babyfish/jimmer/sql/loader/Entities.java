44960ab7d7dc625b7d226cd0d198a0bab5964981
babyfish-ct <babyfish.ct@gmail.com>
2022-07-04 20:23:52 +0800

    Fix bug of DraftImplGenerator

--- project/jimmer-sql/src/test/java/org/babyfish/jimmer/sql/loader/Entities.java
+++ project/jimmer-sql/src/test/java/org/babyfish/jimmer/sql/loader/Entities.java
@@ -2,13 +2,13 @@ package org.babyfish.jimmer.sql.loader;
 
 import org.babyfish.jimmer.runtime.ImmutableSpi;
 import org.babyfish.jimmer.sql.model.BookDraft;
+import org.babyfish.jimmer.sql.model.BookStoreDraft;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import static org.babyfish.jimmer.sql.common.Constants.*;
-import static org.babyfish.jimmer.sql.common.Constants.graphQLInActionId2;
 
 public class Entities {
 
@@ -36,4 +36,17 @@ public class Entities {
                     )
                 )
         );
+
+    @SuppressWarnings("unchecked")
+    public static final List<ImmutableSpi> BOOK_STORES_WITH_ONE_TO_MANY =
+            Collections.unmodifiableList(
+                    (List<ImmutableSpi>) (List<?>) Arrays.asList(
+                            BookStoreDraft.$.produce(store ->
+                                    store.setId(oreillyId)
+                            ),
+                            BookStoreDraft.$.produce(store ->
+                                    store.setId(manningId)
+                            )
+                    )
+            );
 }
--- project/jimmer-sql/src/test/java/org/babyfish/jimmer/sql/loader/ManyToOneWithCacheTest.java
+++ project/jimmer-sql/src/test/java/org/babyfish/jimmer/sql/loader/ManyToOneWithCacheTest.java
@@ -101,4 +101,64 @@ public class ManyToOneWithCacheTest extends AbstractCachedLoaderTest {
             );
         }
     }
+
+    @Test
+    public void loadParentDetail() {
+        Fetcher<Book> fetcher = BookFetcher.$.store(
+                BookStoreFetcher.$.name()
+        );
+        for (int i = 0; i < 2; i++) {
+            boolean useSql = i == 0;
+            connectAndExpect(
+                    con -> new DataLoader(getCachedSqlClient(), con, fetcher.getFieldMap().get("store"))
+                            .load(Entities.BOOKS_WITH_MANY_TO_ONE),
+                    ctx -> {
+                        if (useSql) {
+                            ctx.sql(
+                                    "select tb_1_.ID, tb_1_.STORE_ID " +
+                                            "from BOOK as tb_1_ " +
+                                            "where tb_1_.ID in (?, ?) " +
+                                            "and tb_1_.STORE_ID is not null"
+                            ).variables(learningGraphQLId2, graphQLInActionId2);
+                            ctx.statement(1).sql(
+                                    "select tb_1_.ID, tb_1_.NAME, tb_1_.WEBSITE, tb_1_.VERSION " +
+                                            "from BOOK_STORE as tb_1_ " +
+                                            "where tb_1_.ID in (?, ?)"
+                            ).variables(oreillyId, manningId);
+                        }
+                        ctx.rows(1);
+                        ctx.row(0, map -> {
+                            expect(
+                                    "{" +
+                                            "--->\"id\":\"d38c10da-6be8-4924-b9b9-5e81899612a0\"," +
+                                            "--->\"name\":\"O'REILLY\"" +
+                                            "}",
+                                    map.get(Entities.BOOKS_WITH_MANY_TO_ONE.get(0))
+                            );
+                            expect(
+                                    "{" +
+                                            "--->\"id\":\"d38c10da-6be8-4924-b9b9-5e81899612a0\"," +
+                                            "--->\"name\":\"O'REILLY\"" +
+                                            "}",
+                                    map.get(Entities.BOOKS_WITH_MANY_TO_ONE.get(1))
+                            );
+                            expect(
+                                    "{" +
+                                            "--->\"id\":\"2fa3955e-3e83-49b9-902e-0465c109c779\"," +
+                                            "--->\"name\":\"MANNING\"" +
+                                            "}",
+                                    map.get(Entities.BOOKS_WITH_MANY_TO_ONE.get(2))
+                            );
+                            expect(
+                                    "{" +
+                                            "--->\"id\":\"2fa3955e-3e83-49b9-902e-0465c109c779\"," +
+                                            "--->\"name\":\"MANNING\"" +
+                                            "}",
+                                    map.get(Entities.BOOKS_WITH_MANY_TO_ONE.get(3))
+                            );
+                        });
+                    }
+            );
+        }
+    }
 }
