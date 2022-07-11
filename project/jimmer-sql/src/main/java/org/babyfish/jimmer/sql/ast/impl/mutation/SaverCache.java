438ce759b665e857c7ca567e3f6d3f10ffc1f3a0
babyfish-ct <babyfish.ct@gmail.com>
2022-07-11 19:25:35 +0800

    Fixed issue of save command, add internal TypeId and TypedKey

---
+++ project/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/ast/impl/mutation/SaverCache.java
@@ -0,0 +1,21 @@
+package org.babyfish.jimmer.sql.ast.impl.mutation;
+
+import org.babyfish.jimmer.meta.ImmutableProp;
+import org.babyfish.jimmer.meta.ImmutableType;
+
+import java.util.Set;
+
+class SaverCache extends MutationCache {
+
+    private AbstractEntitySaveCommandImpl.Data data;
+
+    public SaverCache(AbstractEntitySaveCommandImpl.Data data) {
+        super(data.getSqlClient());
+        this.data = data;
+    }
+
+    @Override
+    protected Set<ImmutableProp> keyProps(ImmutableType type) {
+        return data.getKeyProps(type);
+    }
+}
--- project/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/fetcher/impl/FetchingCache.java
+++ project/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/fetcher/impl/FetchingCache.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-public class DataCache {
+public class FetchingCache {
 
     private static final Object NULL = new Object();
 
