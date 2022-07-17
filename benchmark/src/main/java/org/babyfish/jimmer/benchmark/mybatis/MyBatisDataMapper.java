17036edef9745e16c87fa5f71e1c14e98190eace
babyfish-ct <babyfish.ct@gmail.com>
2022-07-18 02:34:24 +0800

    Use JMH to do benchmark

--- benchmark/src/main/java/org/babyfish/jimmer/benchmark/mybatis/MyBatisDataMapper.java
+++ benchmark/src/main/java/org/babyfish/jimmer/benchmark/mybatis/MyBatisDataMapper.java
@@ -8,7 +8,7 @@ import org.apache.ibatis.annotations.Select;
 import java.util.List;
 
 @Mapper
-public interface DataMapper {
+public interface MyBatisDataMapper {
 
     @Select("SELECT * FROM DATA")
     @Results({
