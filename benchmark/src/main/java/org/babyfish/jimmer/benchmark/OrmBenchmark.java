111fe01bd071fb39af2b6beaff7cb76db2fbd823
babyfish-ct <babyfish.ct@gmail.com>
2022-07-18 22:04:01 +0800

    Remove dataCount = 1 of benchmark

--- benchmark/src/main/java/org/babyfish/jimmer/benchmark/OrmBenchmark.java
+++ benchmark/src/main/java/org/babyfish/jimmer/benchmark/OrmBenchmark.java
@@ -28,7 +28,7 @@ import java.sql.SQLException;
 @State(Scope.Benchmark)
 public class OrmBenchmark {
 
-    @Param({"1", "10", "20", "50", "100", "200", "500", "1000"})
+    @Param({"10", "20", "50", "100", "200", "500", "1000"})
     private int dataCount;
 
     private SqlClient sqlClient;
