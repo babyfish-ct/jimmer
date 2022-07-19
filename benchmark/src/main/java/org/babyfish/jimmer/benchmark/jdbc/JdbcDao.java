f0025a7005afe28d85adbb3d6360546cefbfb267
babyfish-ct <babyfish.ct@gmail.com>
2022-07-19 21:35:15 +0800

    Add pure jdbc to benchmark

---
+++ benchmark/src/main/java/org/babyfish/jimmer/benchmark/jdbc/JdbcDao.java
@@ -0,0 +1,66 @@
+package org.babyfish.jimmer.benchmark.jdbc;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JdbcDao {
+
+    private final DataSource dataSource;
+
+    public JdbcDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public List<JdbcData> findAllByColumnIndex() throws SQLException {
+        List<JdbcData> list = new ArrayList<>();
+        try (Connection con = dataSource.getConnection()) {
+            try (PreparedStatement stmt = con.prepareStatement("select * from data")) {
+                try (ResultSet rs = stmt.executeQuery()) {
+                    while (rs.next()) {
+                        JdbcData data = new JdbcData();
+                        data.setId(rs.getLong(1));
+                        data.setValue1(rs.getInt(2));
+                        data.setValue2(rs.getInt(3));
+                        data.setValue3(rs.getInt(4));
+                        data.setValue4(rs.getInt(5));
+                        data.setValue5(rs.getInt(6));
+                        data.setValue6(rs.getInt(7));
+                        data.setValue7(rs.getInt(8));
+                        data.setValue8(rs.getInt(9));
+                        data.setValue9(rs.getInt(10));
+                    }
+                }
+            }
+        }
+        return list;
+    }
+
+    public List<JdbcData> findAllByColumnName() throws SQLException {
+        List<JdbcData> list = new ArrayList<>();
+        try (Connection con = dataSource.getConnection()) {
+            try (PreparedStatement stmt = con.prepareStatement("select * from data")) {
+                try (ResultSet rs = stmt.executeQuery()) {
+                    while (rs.next()) {
+                        JdbcData data = new JdbcData();
+                        data.setId(rs.getLong("ID"));
+                        data.setValue1(rs.getInt("VALUE_1"));
+                        data.setValue1(rs.getInt("VALUE_2"));
+                        data.setValue1(rs.getInt("VALUE_3"));
+                        data.setValue1(rs.getInt("VALUE_4"));
+                        data.setValue1(rs.getInt("VALUE_5"));
+                        data.setValue1(rs.getInt("VALUE_6"));
+                        data.setValue1(rs.getInt("VALUE_7"));
+                        data.setValue1(rs.getInt("VALUE_8"));
+                        data.setValue1(rs.getInt("VALUE_9"));
+                    }
+                }
+            }
+        }
+        return list;
+    }
+}
---
+++ benchmark/src/main/java/org/babyfish/jimmer/benchmark/jdbc/JdbcData.java
@@ -0,0 +1,104 @@
+package org.babyfish.jimmer.benchmark.jdbc;
+
+public class JdbcData {
+
+    private long id;
+
+    private int value1;
+
+    private int value2;
+
+    private int value3;
+
+    private int value4;
+
+    private int value5;
+
+    private int value6;
+
+    private int value7;
+
+    private int value8;
+
+    private int value9;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getValue1() {
+        return value1;
+    }
+
+    public void setValue1(int value1) {
+        this.value1 = value1;
+    }
+
+    public int getValue2() {
+        return value2;
+    }
+
+    public void setValue2(int value2) {
+        this.value2 = value2;
+    }
+
+    public int getValue3() {
+        return value3;
+    }
+
+    public void setValue3(int value3) {
+        this.value3 = value3;
+    }
+
+    public int getValue4() {
+        return value4;
+    }
+
+    public void setValue4(int value4) {
+        this.value4 = value4;
+    }
+
+    public int getValue5() {
+        return value5;
+    }
+
+    public void setValue5(int value5) {
+        this.value5 = value5;
+    }
+
+    public int getValue6() {
+        return value6;
+    }
+
+    public void setValue6(int value6) {
+        this.value6 = value6;
+    }
+
+    public int getValue7() {
+        return value7;
+    }
+
+    public void setValue7(int value7) {
+        this.value7 = value7;
+    }
+
+    public int getValue8() {
+        return value8;
+    }
+
+    public void setValue8(int value8) {
+        this.value8 = value8;
+    }
+
+    public int getValue9() {
+        return value9;
+    }
+
+    public void setValue9(int value9) {
+        this.value9 = value9;
+    }
+}
