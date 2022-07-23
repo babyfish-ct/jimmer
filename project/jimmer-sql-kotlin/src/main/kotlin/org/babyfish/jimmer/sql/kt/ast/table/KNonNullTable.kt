2c56799eb0e0d66db580ef5917ec1d6e64c48cfa
babyfish-ct <babyfish.ct@gmail.com>
2022-07-24 02:27:37 +0800

    Add ksp for jimmer-sql-kotlin

--- project/jimmer-sql-kotlin/src/main/kotlin/org/babyfish/jimmer/sql/kt/ast/table/KNonNullTable.kt
+++ project/jimmer-sql-kotlin/src/main/kotlin/org/babyfish/jimmer/sql/kt/ast/table/KNonNullTable.kt
@@ -1,6 +1,8 @@
 package org.babyfish.jimmer.sql.kt.ast.table
 
-interface KNonNullTable<E: Any> : KTable<E> {
+import org.babyfish.jimmer.sql.ast.Selection
+
+interface KNonNullTable<E: Any> : KTable<E>, Selection<E> {
     override fun <X: Any> join(prop: String): KNonNullTable<X>
     override fun <X: Any> outerJoin(prop: String): KNullableTable<X>
     override fun <X: Any> inverseJoin(prop: String): KNonNullTable<X>
--- project/jimmer-sql-kotlin/src/main/kotlin/org/babyfish/jimmer/sql/kt/ast/table/KNullableTable.kt
+++ project/jimmer-sql-kotlin/src/main/kotlin/org/babyfish/jimmer/sql/kt/ast/table/KNullableTable.kt
@@ -1,6 +1,8 @@
 package org.babyfish.jimmer.sql.kt.ast.table
 
-interface KNullableTable<E: Any> : KTable<E> {
+import org.babyfish.jimmer.sql.ast.Selection
+
+interface KNullableTable<E: Any> : KTable<E>, Selection<E?> {
     override fun <X: Any> join(prop: String): KNullableTable<X>
     override fun <X: Any> inverseJoin(prop: String): KNullableTable<X>
 }
