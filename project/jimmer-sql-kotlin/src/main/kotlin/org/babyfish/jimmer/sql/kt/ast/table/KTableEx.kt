5189645b7ac77020a8021d73a6a6671fcd9ae723
babyfish-ct <babyfish.ct@gmail.com>
2022-08-01 03:28:37 +0800

    1. Enhance KTable; 2. Change join documentation

--- project/jimmer-sql-kotlin/src/main/kotlin/org/babyfish/jimmer/sql/kt/ast/table/KTableEx.kt
+++ project/jimmer-sql-kotlin/src/main/kotlin/org/babyfish/jimmer/sql/kt/ast/table/KTableEx.kt
@@ -1,10 +1,23 @@
 package org.babyfish.jimmer.sql.kt.ast.table
 
 import kotlin.reflect.KClass
+import kotlin.reflect.KProperty1
 
 interface KTableEx<E: Any> : KTable<E> {
+
     override fun <X: Any> join(prop: String): KTableEx<X>
+    override fun <X: Any> joinReference(prop: KProperty1<E, X?>): KTableEx<X>
+    override fun <X: Any> joinList(prop: KProperty1<E, List<X>>): KTableEx<X>
+
     override fun <X: Any> outerJoin(prop: String): KNullableTableEx<X>
+    override fun <X: Any> outerJoinReference(prop: KProperty1<E, X?>): KNullableTableEx<X>
+    override fun <X: Any> outerJoinList(prop: KProperty1<E, List<X>>): KNullableTableEx<X>
+
     override fun <X: Any> inverseJoin(targetType: KClass<X>, backProp: String): KTableEx<X>
+    override fun <X: Any> inverseJoinReference(backProp: KProperty1<X, E?>): KTableEx<X>
+    override fun <X: Any> inverseJoinList(backProp: KProperty1<X, List<E>>): KTableEx<X>
+
     override fun <X: Any> inverseOuterJoin(targetType: KClass<X>, backProp: String): KNullableTableEx<X>
+    override fun <X: Any> inverseOuterJoinReference(backProp: KProperty1<X, E?>): KNullableTableEx<X>
+    override fun <X: Any> inverseOuterJoinList(backProp: KProperty1<X, List<E>>): KNullableTableEx<X>
 }
--- project/jimmer-sql-kotlin/src/main/kotlin/org/babyfish/jimmer/sql/kt/ast/table/impl/KTableExImpl.kt
+++ project/jimmer-sql-kotlin/src/main/kotlin/org/babyfish/jimmer/sql/kt/ast/table/impl/KTableExImpl.kt
@@ -7,8 +7,10 @@ import org.babyfish.jimmer.sql.ast.impl.table.TableImplementor
 import org.babyfish.jimmer.sql.ast.impl.table.TableSelection
 import org.babyfish.jimmer.sql.kt.ast.table.KNullableTableEx
 import org.babyfish.jimmer.sql.kt.ast.table.KTableEx
+import org.babyfish.jimmer.sql.kt.impl.toImmutableProp
 import org.babyfish.jimmer.sql.runtime.SqlBuilder
 import kotlin.reflect.KClass
+import kotlin.reflect.KProperty1
 
 internal abstract class KTableExImpl<E: Any>(
     protected val javaTable: TableImplementor<E>
@@ -17,8 +19,38 @@ internal abstract class KTableExImpl<E: Any>(
     override fun <X : Any> outerJoin(prop: String): KNullableTableEx<X> =
         KNullableTableExImpl(javaTable.join(prop, JoinType.LEFT))
 
+    override fun <X : Any> outerJoinReference(prop: KProperty1<E, X?>): KNullableTableEx<X> =
+        KNullableTableExImpl(javaTable.join(prop.name, JoinType.LEFT))
+
+    override fun <X : Any> outerJoinList(prop: KProperty1<E, List<X>>): KNullableTableEx<X> =
+        KNullableTableExImpl(javaTable.join(prop.name, JoinType.LEFT))
+
     override fun <X : Any> inverseOuterJoin(targetType: KClass<X>, backProp: String): KNullableTableEx<X> =
-        KNullableTableExImpl(javaTable.inverseJoin(targetType.java, backProp))
+        KNullableTableExImpl(
+            javaTable.inverseJoin(
+                targetType.java,
+                backProp,
+                JoinType.LEFT
+            )
+        )
+
+    override fun <X : Any> inverseOuterJoinReference(backProp: KProperty1<X, E?>): KNullableTableEx<X> =
+        KNullableTableExImpl(
+            javaTable.inverseJoin(
+                backProp.toImmutableProp().declaringType.javaClass,
+                backProp.name,
+                JoinType.LEFT
+            )
+        )
+
+    override fun <X : Any> inverseOuterJoinList(backProp: KProperty1<X, List<E>>): KNullableTableEx<X> =
+        KNullableTableExImpl(
+            javaTable.inverseJoin(
+                backProp.toImmutableProp().declaringType.javaClass,
+                backProp.name,
+                JoinType.LEFT
+            )
+        )
 
     override fun accept(visitor: AstVisitor) {
         javaTable.accept(visitor)
