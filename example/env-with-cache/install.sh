f8eca17500ee39299ce93edd75e396f34c98fc17
babyfish-ct <babyfish.ct@gmail.com>
2022-08-03 02:56:06 +0800

    Add docker environment for demo with cache

---
+++ example/env-with-cache/install.sh
@@ -0,0 +1,57 @@
+docker network create -d bridge jimmer-demo-network
+
+docker pull mysql
+docker run \
+	--restart=always \
+	-d \
+	--name jimmer-demo-mysql \
+	--network jimmer-demo-network \
+	--network-alias jimmer-demo-mysql \
+	-p 4001:3306 \
+	-e MYSQL_ROOT_PASSWORD=123456 \
+	mysql
+docker cp mysql.conf jimmer-demo-mysql:/etc/
+docker restart jimmer-demo-mysql
+docker cp maxwell.sql jimmer-demo-mysql:/var/maxwell.sql
+docker exec jimmer-demo-mysql /bin/sh -c \
+	'mysql -uroot -p123456 </var/maxwell.sql'
+
+docker pull zookeeper
+docker run \
+	--restart=always \
+	-d \
+	--network jimmer-demo-network \
+        --network-alias jimmer-demo-zookeeper \
+	--name jimmer-demo-zookeeper \
+	-p 4002:2181 \
+	-v /etc/localtime:/etc/localtime \
+	zookeeper
+
+docker pull ubuntu/kafka
+docker run \
+	--restart=always \
+	-d \
+	--network jimmer-demo-network \
+	--network-alias jimmer-demo-kafka \
+	--name jimmer-demo-kafka \
+	-p 4003:9092 \
+        -e ZOOKEEPER_HOST=jimmer-demo-zookeeper \
+        ubuntu/kafka 
+docker exec jimmer-demo-kafka /bin/bash -c \
+	'/opt/kafka/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 5 --topic maxwell'
+
+docker pull zendesk/maxwell
+docker run \
+	--restart=always \
+	-d \
+	-it \
+        --network jimmer-demo-network \
+        --name maxwell \
+	zendesk/maxwell \
+	bin/maxwell \
+	--user=maxwell \
+	--password=123456 \
+	--host=jimmer-demo-mysql \
+	--producer=kafka \
+	--kafka.bootstrap.servers=jimmer-demo-kafka:9092 \
+	--kafka_topic=maxwell
---
+++ example/env-with-cache/maxwell.sql
@@ -0,0 +1,3 @@
+CREATE USER 'maxwell'@'%' IDENTIFIED BY '123456';
+GRANT ALL ON maxwell.* TO 'maxwell'@'%';
+GRANT SELECT, REPLICATION CLIENT, REPLICATION SLAVE ON *.* TO 'maxwell'@'%';
