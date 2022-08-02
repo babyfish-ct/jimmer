9577c47794159085c622296157c857045b01556d
babyfish-ct <babyfish.ct@gmail.com>
2022-08-03 03:13:50 +0800

    Docker shell is OK

--- example/env-with-cache/install.sh
+++ example/env-with-cache/install.sh
@@ -13,6 +13,8 @@ docker run \
 docker cp mysql.conf jimmer-demo-mysql:/etc/
 docker restart jimmer-demo-mysql
 docker cp maxwell.sql jimmer-demo-mysql:/var/maxwell.sql
+echo "WAIT mysql for 5 seconds"
+sleep 5
 docker exec jimmer-demo-mysql /bin/sh -c \
 	'mysql -uroot -p123456 </var/maxwell.sql'
 
@@ -37,6 +39,8 @@ docker run \
 	-p 4003:9092 \
         -e ZOOKEEPER_HOST=jimmer-demo-zookeeper \
         ubuntu/kafka 
+echo "Wait kafka for 5 seconds"
+sleep 5
 docker exec jimmer-demo-kafka /bin/bash -c \
 	'/opt/kafka/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 5 --topic maxwell'
 
@@ -46,7 +50,7 @@ docker run \
 	-d \
 	-it \
         --network jimmer-demo-network \
-        --name maxwell \
+        --name jimmer-demo-maxwell \
 	zendesk/maxwell \
 	bin/maxwell \
 	--user=maxwell \
---
+++ example/env-with-cache/uninstall.sh
@@ -0,0 +1,9 @@
+docker stop jimmer-demo-maxwell
+docker rm jimmer-demo-maxwell
+docker stop jimmer-demo-kafka
+docker rm jimmer-demo-kafka
+docker stop jimmer-demo-zookeeper
+docker rm jimmer-demo-zookeeper
+docker stop jimmer-demo-mysql
+docker rm jimmer-demo-mysql
+docker network rm jimmer-demo-network
