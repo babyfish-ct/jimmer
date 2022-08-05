8b2878f06aae92b769d66e892a5c772f552ed3d0
babyfish-ct <babyfish.ct@gmail.com>
2022-08-06 04:17:09 +0800

    Can receive kafka message

--- example/env-with-cache/install.sh
+++ example/env-with-cache/install.sh
@@ -33,20 +33,21 @@ docker run \
 	-v /etc/localtime:/etc/localtime \
 	zookeeper
 
-docker pull ubuntu/kafka
+docker pull wurstmeister/kafka
 docker run \
 	--restart=always \
 	-d \
 	--network jimmer-demo-network \
 	--network-alias jimmer-demo-kafka \
 	--name jimmer-demo-kafka \
-	-p 4003:9092 \
-        -e ZOOKEEPER_HOST=jimmer-demo-zookeeper \
-        ubuntu/kafka 
-echo "Wait kafka for 5 seconds"
-sleep 5
-docker exec jimmer-demo-kafka /bin/bash -c \
-	'/opt/kafka/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 5 --topic maxwell'
+	-p 4003:4003 \
+        -e KAFKA_ZOOKEEPER_CONNECT=jimmer-demo-zookeeper:2181 \
+	-e KAFKA_ADVERTISED_LISTENERS=INSIDE://jimmer-demo-kafka:9092,OUTSIDE://localhost:4003 \
+	-e KAFKA_LISTENERS=INSIDE://:9092,OUTSIDE://:4003 \
+	-e KAFKA_LISTENER_SECURITY_PROTOCOL_MAP=INSIDE:PLAINTEXT,OUTSIDE:PLAINTEXT \
+	-e KAFKA_INTER_BROKER_LISTENER_NAME=INSIDE \
+	-e KAFKA_CREATE_TOPICS=maxwell:5:1 \
+        wurstmeister/kafka
 
 docker pull zendesk/maxwell
 docker run \
@@ -62,7 +63,8 @@ docker run \
 	--host=jimmer-demo-mysql \
 	--producer=kafka \
 	--kafka.bootstrap.servers=jimmer-demo-kafka:9092 \
-	--kafka_topic=maxwell
+	--kafka_topic=maxwell \
+	--producer_partition_by=primary_key
 
 docker pull redis
 docker run \
--- example/java/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/example/cache/CaffeineBinder.java
+++ example/java/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/example/cache/CaffeineBinder.java
@@ -3,12 +3,12 @@ package org.babyfish.jimmer.sql.example.cache;
 import com.github.benmanes.caffeine.cache.CacheLoader;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.LoadingCache;
-import org.babyfish.jimmer.sql.example.cache.chain.CacheChain;
-import org.babyfish.jimmer.sql.example.cache.chain.LoadingBinder;
-import org.checkerframework.checker.nullness.qual.Nullable;
+import org.babyfish.jimmer.sql.cache.chain.CacheChain;
+import org.babyfish.jimmer.sql.cache.chain.LoadingBinder;
 
 import java.time.Duration;
 import java.util.*;
+import java.util.stream.Collectors;
 
 // Level-1 Cache
 public class CaffeineBinder<K, V> implements LoadingBinder<K, V> {
@@ -32,17 +32,31 @@ public class CaffeineBinder<K, V> implements LoadingBinder<K, V> {
                 .maximumSize(maximumSize)
                 .expireAfterWrite(duration)
                 .build(
-                        new CacheLoader<K, V>() {
+                        new CacheLoader<K, Optional<V>>() {
 
                             @Override
-                            public Optional<V> load(K key) throws Exception {
-                                return chain.loadAll(Collections.singleton(key)).get(key);
+                            public Optional<V> load(K key) {
+                                Map<K, V> map = chain.loadAll(Collections.singleton(key));
+                                V value = map.get(key);
+                                if (value != null || map.containsKey(key)) {
+                                    return Optional.ofNullable(value);
+                                }
+                                return Optional.ofNullable(null);
                             }
 
                             @SuppressWarnings("unchecked")
                             @Override
-                            public Map<? extends K, ? extends V> loadAll(Set<? extends K> keys) throws Exception {
-                                return chain.loadAll((Set<K>)keys);
+                            public Map<? extends K, ? extends Optional<V>> loadAll(Set<? extends K> keys) {
+                                Map<K, V> map = chain.loadAll((Collection<K>) keys);
+                                return map
+                                        .entrySet()
+                                        .stream()
+                                        .collect(
+                                                Collectors.toMap(
+                                                        Map.Entry::getKey,
+                                                        e -> Optional.ofNullable(e.getValue())
+                                                )
+                                        );
                             }
                         }
                 );
@@ -50,7 +64,12 @@ public class CaffeineBinder<K, V> implements LoadingBinder<K, V> {
 
     @Override
     public Map<K, V> getAll(Collection<K> keys) {
-        return loadingCache.getAll(keys);
+        Map<K, Optional<V>> map = loadingCache.getAll(keys);
+        Map<K, V> convertedMap = new HashMap<>((map.size() * 4 + 2) / 3);
+        for (Map.Entry<K, Optional<V>> e : map.entrySet()) {
+            convertedMap.put(e.getKey(), e.getValue().orElse(null));
+        }
+        return convertedMap;
     }
 
     @Override
