13e10c3c26ad85501328a6dbb880bd214e9bbcfe
babyfish-ct <babyfish.ct@gmail.com>
2022-08-18 19:33:39 +0800

    Change home page

--- doc/src/components/HomepageFeatures/index.tsx
+++ doc/src/components/HomepageFeatures/index.tsx
@@ -27,7 +27,7 @@ const FeatureList: FeatureItem[] = [
         Powerful ORM for immutable data model
         <ul>
           <li>More powerful than other popular ORM frameworks</li>
-          <li>Faster than other other popular frameworks</li>
+          <li>Faster than other popular ORM frameworks</li>
         </ul>
       </div>
     ),
