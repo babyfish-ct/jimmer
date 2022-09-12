66d623f910e71aab552c2d4870adfc30eafd36fc
babyfish-ct <babyfish.ct@gmail.com>
2022-09-12 20:43:37 +0800

    Add lombok to java projects

--- example/java/jimmer-sql-graphql/src/main/java/org/babyfish/jimmer/sql/example/graphql/cfg/SqlClientConfig.java
+++ example/java/jimmer-sql-graphql/src/main/java/org/babyfish/jimmer/sql/example/graphql/cfg/SqlClientConfig.java
@@ -1,5 +1,6 @@
 package org.babyfish.jimmer.sql.example.graphql.cfg;
 
+import lombok.extern.slf4j.Slf4j;
 import org.babyfish.jimmer.sql.JSqlClient;
 import org.babyfish.jimmer.sql.cache.CacheFactory;
 import org.babyfish.jimmer.sql.dialect.H2Dialect;
@@ -7,12 +8,8 @@ import org.babyfish.jimmer.sql.dialect.MySqlDialect;
 import org.babyfish.jimmer.sql.example.graphql.entities.Author;
 import org.babyfish.jimmer.sql.example.graphql.entities.Book;
 import org.babyfish.jimmer.sql.example.graphql.entities.BookStore;
-import org.babyfish.jimmer.sql.example.graphql.entities.Gender;
 import org.babyfish.jimmer.sql.runtime.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceUtils;
@@ -30,9 +27,8 @@ import java.util.Optional;
 import java.util.function.Function;
 
 @Configuration
+@Slf4j
 public class SqlClientConfig {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(SqlClientConfig.class);
 
     @Bean
     public JSqlClient sqlClient(
@@ -76,7 +72,7 @@ public class SqlClientConfig {
                                     StatementFactory statementFactory,
                                     SqlFunction<PreparedStatement, R> block
                             ) {
-                                LOGGER.info("Execute sql : \"{}\", with variables: {}", sql, variables);
+                                log.info("Execute sql : \"{}\", with variables: {}", sql, variables);
                                 return DefaultExecutor.INSTANCE.execute(
                                         con,
                                         sql,
