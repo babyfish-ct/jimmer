34c5cc732f398f302836988555d84fad9205949b
babyfish-ct <babyfish.ct@gmail.com>
2022-09-25 23:21:21 +0800

    Add preface to documentation

--- doc/src/components/HomepageFeatures/index.tsx
+++ doc/src/components/HomepageFeatures/index.tsx
@@ -23,14 +23,11 @@ const FeatureList: FeatureItem[] = [
     title: 'ORM for immutable data model',
     Svg: require('@site/static/img/orm.svg').default,
     description: (
-      <div style={{textAlign: 'left'}}>
-        Powerful ORM for immutable data model
-        <ul>
-          <li>Include cache, no DTO required</li>
-          <li>More powerful than other popular ORM frameworks</li>
-          <li>Faster than other popular ORM frameworks</li>
-        </ul>
-      </div>
+      <ul>
+        <li>Include cache, no DTO required</li>
+        <li>More powerful than other popular ORM frameworks</li>
+        <li>Faster than other popular ORM frameworks</li>
+      </ul>
     ),
   },
   {
