30fc7548162cc72a58a896caa53ce01bc412e164
babyfish-ct <babyfish.ct@gmail.com>
2022-09-27 13:27:30 +0800

    Upgrade demo dependency to 0.2.13

--- example/java/jimmer-sql-graphql/src/main/java/org/babyfish/jimmer/sql/example/graphql/cfg/SqlClientConfig.java
+++ example/java/jimmer-sql-graphql/src/main/java/org/babyfish/jimmer/sql/example/graphql/cfg/SqlClientConfig.java
@@ -1,6 +1,5 @@
 package org.babyfish.jimmer.sql.example.graphql.cfg;
 
-import lombok.extern.slf4j.Slf4j;
 import org.babyfish.jimmer.sql.JSqlClient;
 import org.babyfish.jimmer.sql.cache.CacheFactory;
 import org.babyfish.jimmer.sql.dialect.H2Dialect;
@@ -9,6 +8,8 @@ import org.babyfish.jimmer.sql.example.graphql.entities.Author;
 import org.babyfish.jimmer.sql.example.graphql.entities.Book;
 import org.babyfish.jimmer.sql.example.graphql.entities.BookStore;
 import org.babyfish.jimmer.sql.runtime.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,8 +28,9 @@ import java.util.Optional;
 import java.util.function.Function;
 
 @Configuration
-@Slf4j
 public class SqlClientConfig {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SqlClientConfig.class);
 
     @Bean
     public JSqlClient sqlClient(
@@ -72,7 +74,7 @@ public class SqlClientConfig {
                                     StatementFactory statementFactory,
                                     SqlFunction<PreparedStatement, R> block
                             ) {
-                                log.info("Execute sql : \"{}\", with variables: {}", sql, variables);
+                                LOGGER.info("Execute sql : \"{}\", with variables: {}", sql, variables);
                                 return DefaultExecutor.INSTANCE.execute(
                                         con,
                                         sql,
