cb59a8e5cdb9d1a7ff3aac67b8df34ea2ddc42b8
babyfish-ct <babyfish.ct@gmail.com>
2022-09-30 19:39:16 +0800

    Assocaition property in MappedSuperclass does not requires id

--- project/jimmer-sql/src/test/java/org/babyfish/jimmer/sql/model/permission/PermissionBase.java
+++ project/jimmer-sql/src/test/java/org/babyfish/jimmer/sql/model/permission/PermissionBase.java
@@ -1,14 +1,10 @@
 package org.babyfish.jimmer.sql.model.permission;
 
-import org.babyfish.jimmer.sql.Id;
 import org.babyfish.jimmer.sql.ManyToOne;
 import org.babyfish.jimmer.sql.MappedSuperclass;
 
 @MappedSuperclass
 public interface PermissionBase {
-
-    @Id
-    long getId();
 
     @ManyToOne
     Role getRole();
