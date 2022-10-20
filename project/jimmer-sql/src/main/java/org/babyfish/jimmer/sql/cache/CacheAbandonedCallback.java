cff3000e9a6776b9f29841aa7b5f27e6977e46dc
babyfish-ct <babyfish.ct@gmail.com>
2022-10-20 18:47:53 +0800

    0.3.1

---
+++ project/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/cache/CacheAbandonedCallback.java
@@ -0,0 +1,2 @@
+package org.babyfish.jimmer.sql.cache;public class CacheAbandonedCallback {
+}
--- project/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/event/EventType.java
+++
@@ -1,8 +0,0 @@
-package org.babyfish.jimmer.sql.event;
-
-public enum EventType {
-    DELETE, // For EntityEvent and AssociationEvent
-    INSERT, // For EntityEvent and AssociationEvent
-    UPDATE, // Only for EntityEvent
-    EVICT, // Only for AssociationEvent
-}
