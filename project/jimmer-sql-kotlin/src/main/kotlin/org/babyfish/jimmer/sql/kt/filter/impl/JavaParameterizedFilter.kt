86a44be0713f3aa8abd5fc2f90d6b2d918efff7c
babyfish-ct <babyfish.ct@gmail.com>
2022-10-20 18:47:53 +0800

    0.3.1

--- project/jimmer-sql-kotlin/src/main/kotlin/org/babyfish/jimmer/sql/kt/filter/impl/JavaParameterizedFilter.kt
+++ project/jimmer-sql-kotlin/src/main/kotlin/org/babyfish/jimmer/sql/kt/filter/impl/JavaParameterizedFilter.kt
@@ -2,17 +2,17 @@ package org.babyfish.jimmer.sql.kt.filter.impl
 
 import org.babyfish.jimmer.sql.ast.table.Props
 import org.babyfish.jimmer.sql.event.EntityEvent
-import org.babyfish.jimmer.sql.filter.CacheableFilter
-import org.babyfish.jimmer.sql.kt.filter.KCacheableFilter
+import org.babyfish.jimmer.sql.filter.Filter
+import org.babyfish.jimmer.sql.kt.filter.KFilter
 import java.util.*
 
 internal class JavaCacheableFilter(
-    ktFilter: KCacheableFilter<*>
-) : JavaFilter(ktFilter), CacheableFilter<Props> {
+    ktFilter: KFilter.Parameterized<*>
+) : JavaFilter(ktFilter), Filter.Parameterized<Props> {
 
     override fun getParameters(): SortedMap<String, Any>? =
-        (ktFilter as KCacheableFilter<*>).getParameters()
+        (ktFilter as KFilter.Parameterized<*>).getParameters()
 
     override fun isAffectedBy(e: EntityEvent<*>): Boolean =
-        (ktFilter as KCacheableFilter<*>).isAffectedBy(e)
+        (ktFilter as KFilter.Parameterized<*>).isAffectedBy(e)
 }
