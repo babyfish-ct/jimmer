b6b7290d4913ff21da7d0442ccf6ced9fdc31f88
babyfish-ct <babyfish.ct@gmail.com>
2022-10-22 23:01:23 +0800

    0.3.3

--- project/jimmer-sql-kotlin/build.gradle.kts
+++ project/jimmer-sql-kotlin/build.gradle.kts
@@ -2,8 +2,6 @@ plugins {
     kotlin("jvm") version "1.7.10"
     id("com.google.devtools.ksp") version "1.7.10-1.0.6"
     id("org.jetbrains.dokka") version "1.6.10"
-    id("maven-publish")
-    id("signing")
 }
 
 repositories {
@@ -57,58 +55,4 @@ tasks {
 
 tasks.withType<Javadoc>{
     options.encoding = "UTF-8"
-}
-
-// Publish to maven-----------------------------------------------------
-val NEXUS_USERNAME: String by project
-val NEXUS_PASSWORD: String by project
-
-publishing {
-    repositories {
-        maven {
-            credentials {
-                username = NEXUS_USERNAME
-                password = NEXUS_PASSWORD
-            }
-            name = "MavenCentral"
-            url = if (project.version.toString().endsWith("-SNAPSHOT")) {
-                uri("https://oss.sonatype.org/content/repositories/snapshots")
-            } else {
-                uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-            }
-        }
-    }
-    publications {
-        register("mavenJava", MavenPublication::class) {
-            artifactId = "jimmer-sql-kotlin"
-            from(components["java"])
-            pom {
-                name.set("jimmer")
-                description.set("immer for java")
-                url.set("https://github.com/babyfish-ct/jimmer")
-                licenses {
-                    license {
-                        name.set("MIT")
-                        url.set("https://github.com/babyfish-ct/jimmer/blob/main/LICENSE")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("babyfish-ct")
-                        name.set("陈涛")
-                        email.set("babyfish.ct@gmail.com")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:git://github.com/babyfish-ct/jimmer.git")
-                    developerConnection.set("scm:git:ssh://github.com/babyfish-ct/jimmer.git")
-                    url.set("https://github.com//babyfish-ct/jimmer")
-                }
-            }
-        }
-    }
-}
-
-signing {
-    sign(publishing.publications["mavenJava"])
 }
