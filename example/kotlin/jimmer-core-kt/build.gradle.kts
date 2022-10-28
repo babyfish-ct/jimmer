079c5ebe81084978cb753f641566727241d88282
babyfish-ct <babyfish.ct@gmail.com>
2022-10-28 23:49:22 +0800

    0.3.5

--- example/kotlin/jimmer-core-kt/build.gradle.kts
+++ example/kotlin/jimmer-core-kt/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "org.babyfish.jimmer.example.kt"
-version = "0.3.3"
+version = "0.3.5"
 
 repositories {
     mavenCentral()
@@ -14,8 +14,8 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
 
-    implementation("org.babyfish.jimmer:jimmer-core-kotlin:0.3.3")
-    ksp("org.babyfish.jimmer:jimmer-ksp:0.3.3")
+    implementation("org.babyfish.jimmer:jimmer-core-kotlin:0.3.5")
+    ksp("org.babyfish.jimmer:jimmer-ksp:0.3.5")
 }
 
 // Without this configuration, gradle command can still run.
