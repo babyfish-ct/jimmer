17613a20f664841cb583d4c835fa4f2bb33768c0
babyfish-ct <babyfish.ct@gmail.com>
2022-10-31 02:40:36 +0800

    Add SubTestBaseOnOneTable test for fluent query

--- project/jimmer-sql/src/test/java/org/babyfish/jimmer/sql/query/FluentQueryTest.java
+++ project/jimmer-sql/src/test/java/org/babyfish/jimmer/sql/query/FluentQueryTest.java
@@ -1,5 +1,6 @@
 package org.babyfish.jimmer.sql.query;
 
+import org.babyfish.jimmer.sql.ast.Expression;
 import org.babyfish.jimmer.sql.common.AbstractQueryTest;
 import org.babyfish.jimmer.sql.model.AuthorTableEx;
 import org.babyfish.jimmer.sql.model.BookTable;
@@ -166,6 +167,80 @@ public class FluentQueryTest extends AbstractQueryTest {
                                     "--->--->\"name\":\"Learning GraphQL\"," +
                                     "--->--->\"edition\":1,\"price\":50.00,\"store\":{" +
                                     "--->--->\"id\":\"d38c10da-6be8-4924-b9b9-5e81899612a0\"" +
+                                    "--->--->}" +
+                                    "--->}" +
+                                    "]"
+                    );
+                }
+        );
+    }
+
+    @Test
+    public void testSubQueryOfOneTable() {
+
+        BookTable book = BookTable.$;
+        
+        executeAndExpect(
+                getSqlClient()
+                        .createQuery(book)
+                        .where(
+                                Expression.tuple(
+                                        book.name(),
+                                        book.edition()
+                                ).in(
+                                        getSqlClient()
+                                                .createSubQuery(book)
+                                                .groupBy(book.name())
+                                                .select(
+                                                        book.name(),
+                                                        book.edition().max()
+                                                )
+                                )
+                        )
+                        .select(book),
+                ctx -> {
+                    ctx.sql(
+                            "select tb_1_.ID, tb_1_.NAME, tb_1_.EDITION, tb_1_.PRICE, tb_1_.STORE_ID " +
+                                    "from BOOK as tb_1_ " +
+                                    "where (tb_1_.NAME, tb_1_.EDITION) in (" +
+                                    "--->select tb_2_.NAME, max(tb_2_.EDITION) " +
+                                    "--->from BOOK as tb_2_ " +
+                                    "--->group by tb_2_.NAME" +
+                                    ")"
+                    );
+                    ctx.rows(
+                            "[" +
+                                    "--->{" +
+                                    "--->--->\"id\":\"64873631-5d82-4bae-8eb8-72dd955bfc56\"," +
+                                    "--->--->\"name\":\"Learning GraphQL\"," +
+                                    "--->--->\"edition\":3," +
+                                    "--->--->\"price\":51.00," +
+                                    "--->--->\"store\":{" +
+                                    "--->--->--->\"id\":\"d38c10da-6be8-4924-b9b9-5e81899612a0\"" +
+                                    "--->--->}" +
+                                    "--->},{" +
+                                    "--->--->\"id\":\"9eded40f-6d2e-41de-b4e7-33a28b11c8b6\"," +
+                                    "--->--->\"name\":\"Effective TypeScript\"," +
+                                    "--->--->\"edition\":3," +
+                                    "--->--->\"price\":88.00," +
+                                    "--->--->\"store\":{" +
+                                    "--->--->--->\"id\":\"d38c10da-6be8-4924-b9b9-5e81899612a0\"" +
+                                    "--->--->}" +
+                                    "--->},{" +
+                                    "--->--->\"id\":\"782b9a9d-eac8-41c4-9f2d-74a5d047f45a\"," +
+                                    "--->--->\"name\":\"Programming TypeScript\"," +
+                                    "--->--->\"edition\":3," +
+                                    "--->--->\"price\":48.00," +
+                                    "--->--->\"store\":{" +
+                                    "--->--->--->\"id\":\"d38c10da-6be8-4924-b9b9-5e81899612a0\"" +
+                                    "--->--->}" +
+                                    "--->},{" +
+                                    "--->--->\"id\":\"780bdf07-05af-48bf-9be9-f8c65236fecc\"," +
+                                    "--->--->\"name\":\"GraphQL in Action\"," +
+                                    "--->--->\"edition\":3," +
+                                    "--->--->\"price\":80.00," +
+                                    "--->--->\"store\":{" +
+                                    "--->--->--->\"id\":\"2fa3955e-3e83-49b9-902e-0465c109c779\"" +
                                     "--->--->}" +
                                     "--->}" +
                                     "]"
