852aeca958260ca9463dd1654d283af2c1b3bda4
babyfish-ct <babyfish.ct@gmail.com>
2022-11-29 03:11:59 +0800

    Rollback

--- example/java/jimmer-sql-graphql/src/main/java/org/babyfish/jimmer/sql/example/graphql/input/AuthorInput.java
+++ example/java/jimmer-sql-graphql/src/main/java/org/babyfish/jimmer/sql/example/graphql/input/AuthorInput.java
@@ -1,4 +1,4 @@
-package org.babyfish.jimmer.sql.example.graphql.input;
+package org.babyfish.jimmer.sql.example.graphql.entities.input;
 
 import org.babyfish.jimmer.sql.example.graphql.entities.Author;
 import org.babyfish.jimmer.sql.example.graphql.entities.AuthorDraft;
