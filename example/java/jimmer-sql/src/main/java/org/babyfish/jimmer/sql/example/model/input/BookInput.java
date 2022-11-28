9ff4120f86c0358a2e91d6bae240df5bfb32645c
babyfish-ct <babyfish.ct@gmail.com>
2022-11-29 02:57:42 +0800

    0.5.8

--- example/java/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/example/model/input/BookInput.java
+++ example/java/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/example/model/input/BookInput.java
@@ -1,9 +1,35 @@
 package org.babyfish.jimmer.sql.example.model.input;
 
+import org.babyfish.jimmer.ImmutableConverter;
+import org.babyfish.jimmer.ImmutableObjects;
+import org.babyfish.jimmer.sql.example.model.Author;
+import org.babyfish.jimmer.sql.example.model.Book;
+import org.babyfish.jimmer.sql.example.model.BookProps;
+import org.babyfish.jimmer.sql.example.model.BookStore;
+
 import java.math.BigDecimal;
 import java.util.List;
 
 public class BookInput {
+
+    private static final ImmutableConverter<Book, BookInput> BOOK_CONVERTER =
+        ImmutableConverter
+                .newBuilder(Book.class, BookInput.class)
+                .map(BookProps.ID, mapping -> {
+                    mapping.useIf(input -> input.id != null);
+                })
+                .map(BookProps.STORE, mapping -> {
+                    mapping.valueConverter(value ->
+                            ImmutableObjects.makeIdOnly(BookStore.class, value)
+                    );
+                })
+                .mapList(BookProps.AUTHORS, mapping -> {
+                    mapping.elementConverter(element ->
+                            ImmutableObjects.makeIdOnly(Author.class, element)
+                    );
+                })
+                .autoMapOtherScalars() // name, edition, price
+                .build();
 
     private Long id;
 
@@ -39,6 +65,10 @@ public class BookInput {
 
     public List<Long> getAuthorIds() {
         return authorIds;
+    }
+
+    public Book toBook() {
+        return BOOK_CONVERTER.convert(this);
     }
 
     @Override
