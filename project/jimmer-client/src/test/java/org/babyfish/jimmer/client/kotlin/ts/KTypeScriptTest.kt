6c90ba60f409b29fb8f6be780e85bc366154b325
babyfish-ct <babyfish.ct@gmail.com>
2022-12-21 02:54:32 +0800

    Finish kotlin DTO parser

--- project/jimmer-client/src/test/java/org/babyfish/jimmer/client/kotlin/ts/KTypeScriptTest.kt
+++ project/jimmer-client/src/test/java/org/babyfish/jimmer/client/kotlin/ts/KTypeScriptTest.kt
@@ -2,8 +2,10 @@ package org.babyfish.jimmer.client.kotlin.ts
 
 import org.babyfish.jimmer.client.FetchBy
 import org.babyfish.jimmer.client.generator.ts.Context
+import org.babyfish.jimmer.client.generator.ts.DtoWriter
 import org.babyfish.jimmer.client.generator.ts.ServiceWriter
 import org.babyfish.jimmer.client.generator.ts.TypeDefinitionWriter
+import org.babyfish.jimmer.client.kotlin.model.KBook
 import org.babyfish.jimmer.client.kotlin.model.KBookInput
 import org.babyfish.jimmer.client.kotlin.service.KBookService
 import org.babyfish.jimmer.client.meta.Constants
@@ -34,7 +36,16 @@ class KTypeScriptTest {
         println(code)
     }
 
-    private fun createContext(out: OutputStream): Context? {
+    @Test
+    fun testSimpleDto() {
+        val out = ByteArrayOutputStream()
+        val ctx = createContext(out)
+        DtoWriter(ctx, KBook::class.java, ctx.dtoMap.get(KBook::class.java)).flush()
+        val code = out.toString()
+        println(code)
+    }
+
+    private fun createContext(out: OutputStream): Context {
         return Context(Constants.KOTLIN_METADATA, out, "Api", 4)
     }
 }
