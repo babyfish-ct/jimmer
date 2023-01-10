990dc22e972afb60ff479285e2b95d02fa76bf53
babyfish-ct <babyfish.ct@gmail.com>
2023-01-10 23:04:11 +0800

    0.6.13

--- project/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/ast/table/spi/TypedTableProxy.java
+++
@@ -1,9 +0,0 @@
-package org.babyfish.jimmer.sql.ast.table.spi;
-
-import org.babyfish.jimmer.sql.ast.table.TableEx;
-
-public interface TypedTableProxy<E, TEX extends TableEx<E>> extends TableProxy<E> {
-
-    @Override
-    TEX asTableEx();
-}
