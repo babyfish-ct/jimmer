283e735f4d6151c5340860b33dc230d67f5bf7b6
babyfish-ct <babyfish.ct@gmail.com>
2023-01-12 15:53:24 +0800

    Add Comment to @StaticType/@Static/AutoScalarStrategy

--- project/jimmer-core/src/main/java/org/babyfish/jimmer/pojo/AutoScalarStrategy.java
+++ project/jimmer-core/src/main/java/org/babyfish/jimmer/pojo/AutoScalarStrategy.java
@@ -1,7 +1,29 @@
 package org.babyfish.jimmer.pojo;
 
+/**
+ * How to handle scalar fields that are not decorated by `@Static` explicitly.
+ *
+ * For id field, if the entity type has key, it will be mapped as
+ * optional static field implicitly
+ */
 public enum AutoScalarStrategy {
+
+    /**
+     * Mapping all scalar fields of both current type and super types,
+     * except scalar fields decorated by `@Static(enabled = false)`
+     */
     ALL,
+
+    /**
+     * Mapping all scalar fields of current types,
+     * except scalar fields decorated by `@Static(enabled = false)` and
+     * scalar fields declared in super types
+     */
     DECLARED,
+
+    /**
+     * Scalar field will not be mapped automatically,
+     * unless it is decorated by `@Static(enabled = true)` explicitly
+     */
     NONE
 }
