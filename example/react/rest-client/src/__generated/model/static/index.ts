e8444966ed32093e1532df8e4ce7b214f6690a30
babyfish-ct <babyfish.ct@gmail.com>
2023-02-14 16:32:48 +0800

    0.6.40-preview

--- example/react/rest-client/src/__generated/model/static/index.ts
+++ example/react/rest-client/src/__generated/model/static/index.ts
@@ -1,8 +1,8 @@
-export type { Pageable } from './Pageable';
+export type { Page } from './Page';
+export type { BookStoreInput } from './BookStoreInput';
 export type { BookInput } from './BookInput';
 export type { RecursiveTreeInput } from './RecursiveTreeInput';
-export type { Page } from './Page';
-export type { CompositeBookInput } from './CompositeBookInput';
-export type { BookStoreInput } from './BookStoreInput';
 export type { Sort } from './Sort';
+export type { Pageable } from './Pageable';
 export type { AuthorInput } from './AuthorInput';
+export type { CompositeBookInput } from './CompositeBookInput';
