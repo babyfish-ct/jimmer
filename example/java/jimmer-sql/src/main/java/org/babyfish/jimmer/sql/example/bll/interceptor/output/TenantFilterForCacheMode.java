d78fb45e0eb18cc38ff308b1ca5493956c29a323
babyfish-ct <babyfish.ct@gmail.com>
2023-02-18 21:55:14 +0800

    0.6.44-0.7-preview

--- example/java/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/example/bll/interceptor/output/TenantFilterForCacheMode.java
+++ example/java/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/example/bll/interceptor/output/TenantFilterForCacheMode.java
@@ -10,11 +10,25 @@ import org.springframework.stereotype.Component;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
-/*
- * see JSqlClient.Builder.addFilters
- *
- * This bean is only be used when cache is used.
- */
+// -----------------------------
+// If you are a beginner, please ignore this class
+// and view its super class `TenantFilterForNonCacheMode`,
+// for non-cache mode, this class will never be used.
+//
+// When the global filter generates additional query
+// conditions for `Book`, different users will see different data.
+//
+// In this case, all associated properties and calculated properties
+// related to `Book` will be affected, such as `BookStore.books`,
+// `Author.books`, `BookStore.avgPrice`, `BookStore.newestBooks`.
+//
+// Even when running the program with caching enabled, all affected
+// properties will not use the cache, unless multi-view caching is enabled
+// for those affected properties.
+//
+// This class cooperates with `RedisHashBinder` in `CacheConfig`
+// to support multi-view caching together.
+// -----------------------------
 @Component
 @ConditionalOnProperty("spring.redis.host")
 public class TenantFilterForCacheMode
