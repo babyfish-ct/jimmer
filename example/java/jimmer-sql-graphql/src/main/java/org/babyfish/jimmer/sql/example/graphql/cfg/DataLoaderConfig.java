617a10091de88b9e571a75f68f3f8a8852e7a8e9
babyfish-ct <babyfish.ct@gmail.com>
2023-02-20 03:57:21 +0800

    Better graphql implementation

---
+++ example/java/jimmer-sql-graphql/src/main/java/org/babyfish/jimmer/sql/example/graphql/cfg/DataLoaderConfig.java
@@ -0,0 +1,54 @@
+package org.babyfish.jimmer.sql.example.graphql.cfg;
+
+import org.babyfish.jimmer.meta.ImmutableProp;
+import org.babyfish.jimmer.meta.ImmutableType;
+import org.babyfish.jimmer.meta.TargetLevel;
+import org.babyfish.jimmer.meta.impl.TypedPropImpl;
+import org.babyfish.jimmer.sql.JSqlClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.graphql.execution.BatchLoaderRegistry;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+@Configuration
+public class DataLoaderConfig {
+
+    @SuppressWarnings("unchecked")
+    public DataLoaderConfig(BatchLoaderRegistry registry, JSqlClient sqlClient) {
+        for (ImmutableType type : sqlClient.getEntityManager().getAllTypes()) {
+            if (type.isEntity()) {
+                for (ImmutableProp prop : type.getProps().values()) {
+                    if (prop.isReference(TargetLevel.ENTITY)) {
+                        registry.forName(prop.toString()).registerMappedBatchLoader((sources, env) -> {
+                            return Mono.just(
+                                    sqlClient
+                                            .getLoaders()
+                                            .reference(new TypedPropImpl.Reference<>(prop))
+                                            .batchLoad(sources)
+                            );
+                        });
+                    } else if (prop.isReferenceList(TargetLevel.ENTITY)) {
+                        registry.forName(prop.toString()).registerMappedBatchLoader((sources, env) -> {
+                            return Mono.just(
+                                    (Map<Object, Object>) (Map<?, ?>)sqlClient
+                                            .getLoaders()
+                                            .list(new TypedPropImpl.ReferenceList<>(prop))
+                                            .batchLoad(sources)
+                            );
+                        });
+                    } else if (prop.hasTransientResolver()) {
+                        registry.forName(prop.toString()).registerMappedBatchLoader((sources, env) -> {
+                            return Mono.just(
+                                    sqlClient
+                                            .getLoaders()
+                                            .value(new TypedPropImpl.Scalar<>(prop))
+                                            .batchLoad(sources)
+                            );
+                        });
+                    }
+                }
+            }
+        }
+    }
+}
