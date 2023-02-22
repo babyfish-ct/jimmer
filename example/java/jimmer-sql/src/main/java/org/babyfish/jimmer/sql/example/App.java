2659a3079e7a5f812e44c930b727ff6b8417723e
babyfish-ct <babyfish.ct@gmail.com>
2023-02-22 21:00:52 +0800

    0.6.50-0.7-preview

--- example/java/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/example/App.java
+++ example/java/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/example/App.java
@@ -1,9 +1,11 @@
 package org.babyfish.jimmer.sql.example;
 
+import org.babyfish.jimmer.spring.repository.EnableJimmerRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJimmerRepositories("org.babyfish.jimmer.sql.example.dal")
 public class App {
 
     public static void main(String[] args) {
