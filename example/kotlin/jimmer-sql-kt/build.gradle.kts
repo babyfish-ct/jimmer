b2d29b162805603b9a765c18d197c30f61bbd3cf
babyfish-ct <babyfish.ct@gmail.com>
2023-04-18 14:31:37 +0800

    0.7.38

--- example/kotlin/jimmer-sql-kt/build.gradle.kts
+++ example/kotlin/jimmer-sql-kt/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 	id("com.google.devtools.ksp") version "1.7.10-1.0.6"
 }
 
-val jimmerVersion = "0.7.37"
+val jimmerVersion = "0.7.38"
 
 group = "org.babyfish.jimmer.example.kt"
 version = jimmerVersion
