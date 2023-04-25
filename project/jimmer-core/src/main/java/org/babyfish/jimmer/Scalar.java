86aed0d8da1bfeca7356b56ebce80684024983e9
babyfish-ct <babyfish.ct@gmail.com>
2023-04-26 01:13:15 +0800

    Resolve issue #63

--- project/jimmer-core/src/main/java/org/babyfish/jimmer/Scalar.java
+++ project/jimmer-core/src/main/java/org/babyfish/jimmer/Scalar.java
@@ -11,11 +11,12 @@ import java.lang.annotation.Target;
  * Generally speaking, this annotation does not need to be used,
  * and the scalar property does not need to be explicitly stated.
  *
- * If the property is of type `List<E>`, it will be treated as
+ * If the property is of type `List&lt;E&gt;`, it will be treated as
  * special collection property whose element type must be Java class/interface.
  * For example, one-to-many and many-to-many associations, and their `@{@link org.babyfish.jimmer.sql.IdView}` properties.
  *
- * However, sometimes `List<E>` may be just a JSON field, and even nested structures can appear (for example: `List<List<E>>`),
+ * However, sometimes `List&lt;E&gt;` may be just a JSON field, and even nested structures can appear
+ * (for example: `List&lt;List&lt;E&gt;&gt;`),
  * this is the need to explicitly use this annotation or any other annotation decorated by this annotation(eg: Serialized).
  */
 @Retention(RetentionPolicy.RUNTIME)
