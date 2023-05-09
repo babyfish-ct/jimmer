630e84b838ba5757d9e16dd49c6c3105dc92d579
mayihua <1@1.com>
2023-05-09 12:22:05 +0800

    ImmutableModule去除idea反编译的注释

--- project/jimmer-core/src/main/java/org/babyfish/jimmer/jackson/ImmutableModule.java
+++ project/jimmer-core/src/main/java/org/babyfish/jimmer/jackson/ImmutableModule.java
@@ -1,16 +1,10 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package org.babyfish.jimmer.jackson;
 
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
 public class ImmutableModule extends SimpleModule {
-    public ImmutableModule() {
-    }
 
+    @Override
     public void setupModule(SetupContext ctx) {
         super.setupModule(ctx);
         ctx.setClassIntrospector(new JimmerClassIntrospector());
