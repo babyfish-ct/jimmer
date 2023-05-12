653a97eb343900ead64f413199271aefa01f52f7
babyfish-ct <babyfish.ct@gmail.com>
2023-05-13 02:22:49 +0800

    0.7.62

--- project/jimmer-core/src/main/java/org/babyfish/jimmer/JimmerVersion.java
+++ project/jimmer-core/src/main/java/org/babyfish/jimmer/JimmerVersion.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 public class JimmerVersion {
 
     public static final JimmerVersion CURRENT =
-            new JimmerVersion(0, 7, 61);
+            new JimmerVersion(0, 7, 62);
 
     private final int major;
 
