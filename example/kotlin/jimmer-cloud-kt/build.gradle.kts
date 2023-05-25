17fae2c4057551b2a36bfe2d316bc5750310a92e
babyfish-ct <babyfish.ct@gmail.com>
2023-05-25 22:42:02 +0800

    0.7.71

--- example/kotlin/jimmer-cloud-kt/build.gradle.kts
+++ example/kotlin/jimmer-cloud-kt/build.gradle.kts
@@ -1,4 +1,4 @@
-val jimmerVersion by extra { "0.7.70" }
+val jimmerVersion by extra { "0.7.71" }
 
 allprojects {
     group = "org.babyfish.jimmer.example.cloud.kt"
--- example/kotlin/jimmer-core-kt/build.gradle.kts
+++ example/kotlin/jimmer-core-kt/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("com.google.devtools.ksp") version "1.7.10-1.0.6"
 }
 
-val jimmerVersion = "0.7.70"
+val jimmerVersion = "0.7.71"
 
 group = "org.babyfish.jimmer.example.kt"
 version = jimmerVersion
