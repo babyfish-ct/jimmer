b7bf98bfb14b4d41bafffc49c6a904368da7bbe8
babyfish-ct <babyfish.ct@gmail.com>
2023-08-08 04:53:51 +0800

    example/jimmer-sql uses 0.7.130

--- example/java/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/example/business/TreeService.java
+++ example/java/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/example/business/TreeService.java
@@ -3,6 +3,7 @@ package org.babyfish.jimmer.sql.example.business;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.babyfish.jimmer.client.FetchBy;
 import org.babyfish.jimmer.client.ThrowsAll;
+import org.babyfish.jimmer.sql.example.model.dto.FlatTreeNodeView;
 import org.babyfish.jimmer.sql.example.repository.TreeNodeRepository;
 import org.babyfish.jimmer.sql.example.model.TreeNode;
 import org.babyfish.jimmer.sql.example.model.TreeNodeDraft;
@@ -38,6 +39,16 @@ public class TreeService {
 
     public TreeService(TreeNodeRepository treeNodeRepository, ObjectMapper objectMapper) {
         this.treeNodeRepository = treeNodeRepository;
+    }
+
+    @GetMapping("/flatNodes")
+    public List<FlatTreeNodeView> flatNodes(
+            @RequestParam(required = false) String name
+    ) {
+        return treeNodeRepository.findByNameLikeIgnoreCase(
+                name,
+                FlatTreeNodeView.class
+        );
     }
 
     @GetMapping("/roots/recursive")
--- example/java/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/example/repository/TreeNodeRepository.java
+++ example/java/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/example/repository/TreeNodeRepository.java
@@ -1,5 +1,6 @@
 package org.babyfish.jimmer.sql.example.repository;
 
+import org.babyfish.jimmer.View;
 import org.babyfish.jimmer.spring.repository.JRepository;
 import org.babyfish.jimmer.sql.example.model.TreeNode;
 import org.babyfish.jimmer.sql.fetcher.Fetcher;
@@ -8,6 +9,21 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 
 public interface TreeNodeRepository extends JRepository<TreeNode, Long> {
+
+    /*
+     * This approach is very special, the rest query methods of the project returns 'dynamic object + @FetchBy',
+     * but it directly returns static types which should be generated at compilation-time.
+     *
+     * In fact, you can define a method as:
+     * List<FlatTreeNodeView> findByNameLikeIgnoreCase(@Nullable String name)
+     *
+     * However, a better development experience will determine the shape of the data structure at the business layer,
+     * not the data layer. So, let's define the parameter `viewType`
+     */
+    <V extends View<TreeNode>> List<V> findByNameLikeIgnoreCase(
+            @Nullable String name,
+            Class<V> viewType
+    );
 
     List<TreeNode> findByParentIsNullAndName(
             @Nullable String name,
