320b8bf1e050b2f5b4403cd428f77258d9df9487
babyfish-ct <babyfish.ct@gmail.com>
2023-08-14 16:48:49 +0800

    0.8.1

--- example/react/rest-client/src/__generated/index.ts
+++ example/react/rest-client/src/__generated/index.ts
@@ -1,7 +1,7 @@
 export { Api } from './Api';
 export type { ApiErrors, AllErrors } from './ApiErrors';
-export type { Executor } from './Executor';
 export type { Dynamic } from './Dynamic';
+export type { ElementOf } from './ElementOf';
+export type { Executor } from './Executor';
 export type { RequestOf } from './RequestOf';
 export type { ResponseOf } from './ResponseOf';
-export type { ElementOf } from './ElementOf';
