2f65f263375312634e9b5b4e2f271bfe1b6c9966
babyfish-ct <babyfish.ct@gmail.com>
2023-10-24 05:34:11 +0800

    0.8.30

--- example/kotlin/jimmer-sql-kt/build.gradle.kts
+++ example/kotlin/jimmer-sql-kt/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 	id("com.google.devtools.ksp") version "1.7.10-1.0.6"
 }
 
-val jimmerVersion = "0.8.27"
+val jimmerVersion = "0.8.29"
 
 group = "org.babyfish.jimmer.example.kt"
 version = jimmerVersion
--- example/kotlin/jimmer-sql-kt/src/main/kotlin/org/babyfish/jimmer/sql/example/cfg/CorsConfig.kt
+++ example/kotlin/jimmer-sql-kt/src/main/kotlin/org/babyfish/jimmer/sql/example/cfg/CorsConfig.kt
@@ -1,11 +1,5 @@
 package org.babyfish.jimmer.sql.example.cfg
 
-import org.babyfish.jimmer.spring.SpringJSqlClient
-import org.babyfish.jimmer.sql.kt.KSqlClient
-import org.babyfish.jimmer.sql.kt.toKSqlClient
-import org.springframework.context.ApplicationContext
-import org.springframework.context.ApplicationEventPublisher
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 
@@ -20,12 +14,5 @@ class CorsConfig : WebMvcConfigurer {
             .allowedMethods("*")
             .maxAge(1800)
             .allowedOrigins("*")
-    }
-
-    @Bean
-    fun sqlClient(ctx: ApplicationContext?, publisher: ApplicationEventPublisher?): KSqlClient {
-        return object: SpringJSqlClient(ctx, publisher, true) {
-
-        }.toKSqlClient()
     }
 }
