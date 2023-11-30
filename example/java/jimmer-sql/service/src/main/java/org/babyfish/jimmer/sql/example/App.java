414d03110c3b71983ed8f5555ae5c26684dac8fa
babyfish-ct <babyfish.ct@gmail.com>
2023-11-30 09:09:06 +0800

    0.8.51

--- example/java/jimmer-sql/service/src/main/java/org/babyfish/jimmer/sql/example/App.java
+++ example/java/jimmer-sql/service/src/main/java/org/babyfish/jimmer/sql/example/App.java
@@ -1,5 +1,6 @@
 package org.babyfish.jimmer.sql.example;
 
+import org.babyfish.jimmer.client.EnableImplicitApi;
 import org.babyfish.jimmer.sql.EnableDtoGeneration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +16,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * <p>This is only required by java, not kotlin(kotlin uses KSP, not APT). </p>
  */
 @EnableDtoGeneration
+@EnableImplicitApi
 @SpringBootApplication
 public class App {
 
--- example/kotlin/jimmer-sql-kt/build.gradle.kts
+++ example/kotlin/jimmer-sql-kt/build.gradle.kts
@@ -1,4 +1,4 @@
-val jimmerVersion by extra { "0.8.50" }
+val jimmerVersion by extra { "0.8.51" }
 val springBootVersion by extra { "2.7.0" }
 
 allprojects {
