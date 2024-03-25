7b7735307f48664af7718a2bd282d3db45c8776a
Белов Александр Федорович <Belov.A.Fedo@sberbank.ru>
2024-03-25 14:35:59 +0300

    Remove unnecessary imports

--- project/buildSrc/src/main/kotlin/org/babyfish/jimmer/java-convention.gradle.kts
+++ project/buildSrc/src/main/kotlin/org/babyfish/jimmer/java-convention.gradle.kts
@@ -1,10 +1,3 @@
-import org.gradle.api.JavaVersion
-import org.gradle.api.plugins.JavaPluginExtension
-import org.gradle.api.tasks.compile.JavaCompile
-import org.gradle.api.tasks.javadoc.Javadoc
-import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.withType
-
 plugins {
     `java-library`
 }
