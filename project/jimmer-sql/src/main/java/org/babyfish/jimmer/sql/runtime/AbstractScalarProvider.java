16b7dd5f38b14f2502618397eee4a195eaa1bc15
babyfish-ct <babyfish.ct@gmail.com>
2024-04-02 23:01:41 +0800

    Better ScalarProvider

--- project/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/runtime/AbstractScalarProvider.java
+++ project/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/runtime/AbstractScalarProvider.java
@@ -1,23 +1,11 @@
 package org.babyfish.jimmer.sql.runtime;
-
-import org.apache.commons.lang3.reflect.TypeUtils;
-import org.babyfish.jimmer.impl.util.ClassCache;
-import org.babyfish.jimmer.sql.Embeddable;
-import org.babyfish.jimmer.sql.Entity;
-import org.babyfish.jimmer.sql.MappedSuperclass;
-import org.babyfish.jimmer.sql.ast.impl.TupleImplementor;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Type;
-import java.lang.reflect.TypeVariable;
 import java.nio.ByteBuffer;
-import java.util.Map;
 import java.util.UUID;
 
 public abstract class AbstractScalarProvider<T, S> implements ScalarProvider<T, S> {
-
-    static final ClassCache<Meta> META_CACHE =
-            new ClassCache<>(AbstractScalarProvider::createMeta);
 
     static final ScalarProvider<UUID, byte[]> UUID_BY_BYTE_ARRAY =
             new AbstractScalarProvider<UUID, byte[]>() {
@@ -56,14 +44,14 @@ public abstract class AbstractScalarProvider<T, S> implements ScalarProvider<T, 
     private final Class<S> sqlType;
 
     public AbstractScalarProvider(Class<T> scalarType, Class<S> sqlType) {
-        validateScalarType(scalarType);
+        Meta.validateScalarType(scalarType);
         this.scalarType = scalarType;
         this.sqlType = sqlType;
     }
 
     @SuppressWarnings("unchecked")
     protected AbstractScalarProvider() {
-        Meta meta = createMeta(this.getClass());
+        Meta meta = Meta.create(this.getClass());
         this.scalarType = meta.scalarType;
         this.sqlType = (Class<S>) meta.sqlType;
     }
@@ -76,114 +64,5 @@ public abstract class AbstractScalarProvider<T, S> implements ScalarProvider<T, 
     @NotNull
     public final Class<S> getSqlType() {
         return sqlType;
-    }
-
-    private static Meta createMeta(Class<?> scalarProviderType) {
-        Map<TypeVariable<?>, Type> argMap = TypeUtils.getTypeArguments(scalarProviderType, ScalarProvider.class);
-        if (argMap.isEmpty()) {
-            throw new IllegalStateException(
-                    "Illegal type \"" +
-                            scalarProviderType.getName() +
-                            "\", it does not specify generic arguments for \"" +
-                            ScalarProvider.class.getName() +
-                            "\""
-            );
-        }
-        TypeVariable<?>[] params = ScalarProvider.class.getTypeParameters();
-        Type scalarType = argMap.get(params[0]);
-        Class<?> sqlType = (Class<?>) argMap.get(params[1]);
-        validateScalarType(scalarType);
-        return new Meta(scalarType, sqlType);
-    }
-
-    private static void validateScalarType(Type scalarType) {
-
-        if (scalarType == UUID.class) {
-            return; // UUID is standard type, but it can be overridden by ScalarProvider
-        }
-
-        if (!(scalarType instanceof Class<?>)) {
-            return;
-        }
-        Class<?> scalarClass = (Class<?>) scalarType;
-
-        if (scalarType == void.class) {
-            throw new IllegalArgumentException(
-                    "Illegal scalar type \"" +
-                            scalarClass.getName() +
-                            "\", it cannot be void"
-            );
-        }
-        if (scalarType == Object.class) {
-            throw new IllegalArgumentException(
-                    "Illegal scalar type \"" +
-                            scalarClass.getName() +
-                            "\", scalar provider does not support object type which means any"
-            );
-        }
-        if (TupleImplementor.class.isAssignableFrom(scalarClass)) {
-            throw new IllegalArgumentException(
-                    "Illegal scalar type \"" +
-                            scalarClass.getName() +
-                            "\", scalar provider does not support tuple type"
-            );
-        }
-        if (ReaderManager.isStandardScalarType(scalarClass)) {
-            throw new IllegalArgumentException(
-                    "Illegal scalar type \"" +
-                            ((Class<?>)scalarType).getName() +
-                            "\", scalar provider does not support standard scalar type"
-            );
-        }
-        Class<?> annotationType = getOrmAnnotationType(scalarClass);
-        if (annotationType != null) {
-            throw new IllegalArgumentException(
-                    "Illegal scalar type \"" +
-                            scalarClass.getName() +
-                            "\", scalar provider does not support scalar type which is decorated by \"@" +
-                            annotationType.getName() +
-                            "\""
-            );
-        }
-    }
-
-    private static Class<?> getOrmAnnotationType(Class<?> type) {
-        if (type == null) {
-            return null;
-        }
-        if (type != Object.class) {
-            if (type.isAnnotationPresent(Entity.class)) {
-                return Entity.class;
-            }
-            if (type.isAssignableFrom(MappedSuperclass.class)) {
-                return MappedSuperclass.class;
-            }
-            if (type.isAssignableFrom(Embeddable.class)) {
-                return Embeddable.class;
-            }
-        }
-        Class<?> annoType = getOrmAnnotationType(type.getSuperclass());
-        if (annoType != null) {
-            return annoType;
-        }
-        for (Class<?> interfaceType : type.getInterfaces()) {
-            annoType = getOrmAnnotationType(interfaceType);
-            if (annoType != null) {
-                return annoType;
-            }
-        }
-        return null;
-    }
-
-    static class Meta {
-
-        final Type scalarType;
-
-        final Class<?> sqlType;
-
-        private Meta(Type scalarType, Class<?> sqlType) {
-            this.scalarType = scalarType;
-            this.sqlType = sqlType;
-        }
     }
 }
--- project/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/runtime/ScalarProvider.java
+++ project/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/runtime/ScalarProvider.java
@@ -1,10 +1,17 @@
 package org.babyfish.jimmer.sql.runtime;
 
+import org.apache.commons.lang3.reflect.TypeUtils;
+import org.babyfish.jimmer.impl.util.ClassCache;
 import org.babyfish.jimmer.meta.ImmutableProp;
 import org.babyfish.jimmer.meta.TypedProp;
+import org.babyfish.jimmer.sql.Embeddable;
+import org.babyfish.jimmer.sql.Entity;
+import org.babyfish.jimmer.sql.MappedSuperclass;
+import org.babyfish.jimmer.sql.ast.impl.TupleImplementor;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
 import java.util.*;
 import java.util.function.Consumer;
 
@@ -12,13 +19,13 @@ public interface ScalarProvider<T, S> {
 
     @NotNull
     default Type getScalarType() {
-        return AbstractScalarProvider.META_CACHE.get(this.getClass()).scalarType;
+        return Meta.of(this.getClass()).scalarType;
     }
 
     @SuppressWarnings("unchecked")
     @NotNull
     default Class<S> getSqlType() {
-        return (Class<S>) AbstractScalarProvider.META_CACHE.get(this.getClass()).sqlType;
+        return (Class<S>) Meta.of(this.getClass()).sqlType;
     }
 
     T toScalar(@NotNull S sqlValue) throws Exception;
@@ -94,5 +101,129 @@ public interface ScalarProvider<T, S> {
 
     static ScalarProvider<UUID, String> uuidByString() {
         return AbstractScalarProvider.UUID_BY_STRING;
+    }
+
+    class Meta {
+
+        private static final ClassCache<Meta> META_CACHE = new ClassCache<>(Meta::create);
+
+        final Type scalarType;
+
+        final Class<?> sqlType;
+
+        private Meta(Type scalarType, Class<?> sqlType) {
+            this.scalarType = scalarType;
+            this.sqlType = sqlType;
+        }
+
+        public static Meta of(Class<?> scalarProviderType) {
+            return META_CACHE.get(scalarProviderType);
+        }
+
+        static Meta create(Class<?> scalarProviderType) {
+            if (!ScalarProvider.class.isAssignableFrom(scalarProviderType)) {
+                throw new IllegalArgumentException(
+                        "The `scalarProviderType` \"" +
+                                scalarProviderType.getName() +
+                                "\" + does not implement \"" +
+                                ScalarProvider.class.getName() +
+                                "\""
+                );
+            }
+            Map<TypeVariable<?>, Type> argMap = TypeUtils.getTypeArguments(scalarProviderType, ScalarProvider.class);
+            if (argMap.isEmpty()) {
+                throw new IllegalStateException(
+                        "Illegal type \"" +
+                                scalarProviderType.getName() +
+                                "\", it does not specify generic arguments for \"" +
+                                ScalarProvider.class.getName() +
+                                "\""
+                );
+            }
+            TypeVariable<?>[] params = ScalarProvider.class.getTypeParameters();
+            Type scalarType = argMap.get(params[0]);
+            Class<?> sqlType = (Class<?>) argMap.get(params[1]);
+            validateScalarType(scalarType);
+            return new Meta(scalarType, sqlType);
+        }
+
+        static void validateScalarType(Type scalarType) {
+
+            if (scalarType == UUID.class) {
+                return; // UUID is standard type, but it can be overridden by ScalarProvider
+            }
+
+            if (!(scalarType instanceof Class<?>)) {
+                return;
+            }
+            Class<?> scalarClass = (Class<?>) scalarType;
+
+            if (scalarType == void.class) {
+                throw new IllegalArgumentException(
+                        "Illegal scalar type \"" +
+                                scalarClass.getName() +
+                                "\", it cannot be void"
+                );
+            }
+            if (scalarType == Object.class) {
+                throw new IllegalArgumentException(
+                        "Illegal scalar type \"" +
+                                scalarClass.getName() +
+                                "\", scalar provider does not support object type which means any"
+                );
+            }
+            if (TupleImplementor.class.isAssignableFrom(scalarClass)) {
+                throw new IllegalArgumentException(
+                        "Illegal scalar type \"" +
+                                scalarClass.getName() +
+                                "\", scalar provider does not support tuple type"
+                );
+            }
+            if (ReaderManager.isStandardScalarType(scalarClass)) {
+                throw new IllegalArgumentException(
+                        "Illegal scalar type \"" +
+                                ((Class<?>)scalarType).getName() +
+                                "\", scalar provider does not support standard scalar type"
+                );
+            }
+            Class<?> annotationType = getOrmAnnotationType(scalarClass);
+            if (annotationType != null) {
+                throw new IllegalArgumentException(
+                        "Illegal scalar type \"" +
+                                scalarClass.getName() +
+                                "\", scalar provider does not support scalar type which is decorated by \"@" +
+                                annotationType.getName() +
+                                "\""
+                );
+            }
+        }
+
+        private static Class<?> getOrmAnnotationType(Class<?> type) {
+            if (type == null) {
+                return null;
+            }
+            if (type != Object.class) {
+                if (type.isAnnotationPresent(Entity.class)) {
+                    return Entity.class;
+                }
+                if (type.isAssignableFrom(MappedSuperclass.class)) {
+                    return MappedSuperclass.class;
+                }
+                if (type.isAssignableFrom(Embeddable.class)) {
+                    return Embeddable.class;
+                }
+            }
+            Class<?> annoType = getOrmAnnotationType(type.getSuperclass());
+            if (annoType != null) {
+                return annoType;
+            }
+            for (Class<?> interfaceType : type.getInterfaces()) {
+                annoType = getOrmAnnotationType(interfaceType);
+                if (annoType != null) {
+                    return annoType;
+                }
+            }
+            return null;
+        }
     }
 }
