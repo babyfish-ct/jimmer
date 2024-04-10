8679a280981117f7f23c520425f3f31bb6be202c
Белов Александр Федорович <Belov.A.Fedo@sberbank.ru>
2024-04-11 01:48:40 +0300

    Fix jimmer-bom

--- project/jimmer-bom/build.gradle.kts
+++ project/jimmer-bom/build.gradle.kts
@@ -5,16 +5,16 @@ plugins {
 
 dependencies {
     constraints {
-        api(platform(projects.jimmerApt))
-        api(platform(projects.jimmerClient))
-        api(platform(projects.jimmerClientSwagger))
-        api(platform(projects.jimmerCore))
-        api(platform(projects.jimmerCoreKotlin))
-        api(platform(projects.jimmerDtoCompiler))
-        api(platform(projects.jimmerKsp))
-        api(platform(projects.jimmerMapstructApt))
-        api(platform(projects.jimmerSpringBootStarter))
-        api(platform(projects.jimmerSql))
-        api(platform(projects.jimmerSqlKotlin))
+        api(projects.jimmerApt)
+        api(projects.jimmerClient)
+        api(projects.jimmerClientSwagger)
+        api(projects.jimmerCore)
+        api(projects.jimmerCoreKotlin)
+        api(projects.jimmerDtoCompiler)
+        api(projects.jimmerKsp)
+        api(projects.jimmerMapstructApt)
+        api(projects.jimmerSpringBootStarter)
+        api(projects.jimmerSql)
+        api(projects.jimmerSqlKotlin)
     }
 }
