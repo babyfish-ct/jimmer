de2a100248f483b803e579241a0ee9f7d9194998
babyfish-ct <babyfish.ct@gmail.com>
2024-07-19 03:38:57 +0800

    Add doc comment for @Key

--- project/jimmer-core/src/main/java/org/babyfish/jimmer/sql/Key.java
+++ project/jimmer-core/src/main/java/org/babyfish/jimmer/sql/Key.java
@@ -5,7 +5,24 @@ import kotlin.annotation.AnnotationTarget;
 import java.lang.annotation.*;
 
 /**
- * 
+ * In ORM, there are two concepts: Surrogate id and Natural id.
+ *
+ * <ul>
+ *     <li>{@link Id} is used to specify surrogate id</li>
+ *     <li>This annotation is used to specify natural id</li>
+ * </ul>
+ *
+ * <b>Notes</b>
+ * <ol>
+ *     <li>
+ *         Multiple properties based on column(s) of an entity
+ *         can be decorated by this annotation
+ *     </li>
+ *     <li>
+ *         One-to-one/Many-to-one association property
+ *         based on foreign key can be a part of key
+ *     </li>
+ * </ol>
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
