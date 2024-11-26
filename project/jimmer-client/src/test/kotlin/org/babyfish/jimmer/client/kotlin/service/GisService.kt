0f63f85f8e77ebe3043b30372b5982543bc6c750
babyfish-ct <babyfish.ct@gmail.com>
2024-11-26 20:42:57 +0800

    Remove unnecessary change

--- project/jimmer-client/src/test/kotlin/org/babyfish/jimmer/client/kotlin/service/GisService.kt
+++ project/jimmer-client/src/test/kotlin/org/babyfish/jimmer/client/kotlin/service/GisService.kt
@@ -2,7 +2,6 @@ package org.babyfish.jimmer.client.kotlin.service
 
 import org.babyfish.jimmer.client.common.GetMapping
 import org.babyfish.jimmer.client.common.PathVariable
-import org.babyfish.jimmer.client.kotlin.model.GisArea
 import org.babyfish.jimmer.client.kotlin.model.dto.GisAreaView
 import org.babyfish.jimmer.client.meta.Api
 
