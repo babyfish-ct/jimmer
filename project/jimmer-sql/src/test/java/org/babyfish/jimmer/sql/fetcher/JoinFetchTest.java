065fa03b3e77b17bb0077d7f68de02a778033f78
babyfish-ct <babyfish.ct@gmail.com>
2025-01-24 07:25:19 +0800

    Join Fetch

--- project/jimmer-sql/src/test/java/org/babyfish/jimmer/sql/fetcher/JoinFetchTest.java
+++ project/jimmer-sql/src/test/java/org/babyfish/jimmer/sql/fetcher/JoinFetchTest.java
@@ -285,7 +285,7 @@ public class JoinFetchTest extends AbstractQueryTest {
         BookTable table = BookTable.$;
         ConfigurableRootQuery<BookTable, Book> query = getSqlClient()
                 .createQuery(table)
-                .where(table.store().name().eq("O'REILLY"))
+                .where(table.name().ilike("graphql"))
                 .orderBy(table.name(), table.edition())
                 .select(
                         table.fetch(
@@ -294,40 +294,39 @@ public class JoinFetchTest extends AbstractQueryTest {
                                         .store(
                                                 ReferenceFetchType.JOIN_ALWAYS,
                                                 BookStoreFetcher.$
-                                                        .allScalarFields()
+                                                        .name()
                                         )
                         )
                 );
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < 2; i++) {
             connectAndExpect(
                     con -> query.fetchPage(1, 2, con),
                     ctx -> {
                         ctx.sql(
                                 "select count(1) from BOOK tb_1_ " +
-                                        "inner join BOOK_STORE tb_2_ on tb_1_.STORE_ID = tb_2_.ID " +
-                                        "where tb_2_.NAME = ?"
+                                        "where tb_1_.NAME ilike ?"
                         );
                         ctx.statement(1).sql(
                                 "select tb_1_.ID, tb_1_.NAME, tb_1_.EDITION, tb_1_.PRICE, " +
-                                        "tb_2_.ID, tb_2_.NAME, tb_2_.WEBSITE, tb_2_.VERSION " +
+                                        "tb_2_.ID, tb_2_.NAME " +
                                         "from BOOK tb_1_ " +
-                                        "inner join BOOK_STORE tb_2_ on tb_1_.STORE_ID = tb_2_.ID " +
-                                        "where tb_2_.NAME = ? " +
-                                        "order by tb_1_.NAME asc, tb_1_.EDITION asc limit ? offset ?"
+                                        "left join BOOK_STORE tb_2_ on tb_1_.STORE_ID = tb_2_.ID " +
+                                        "where tb_1_.NAME ilike ? " +
+                                        "order by tb_1_.NAME asc, tb_1_.EDITION asc " +
+                                        "limit ? offset ?"
                         );
-                        ctx.rows(
-                                "[{" +
+                        ctx.row(
+                                0,
+                                "{" +
                                         "--->\"rows\":[" +
                                         "--->--->{" +
-                                        "--->--->--->\"id\":\"9eded40f-6d2e-41de-b4e7-33a28b11c8b6\"," +
-                                        "--->--->--->\"name\":\"Effective TypeScript\"," +
+                                        "--->--->--->\"id\":\"780bdf07-05af-48bf-9be9-f8c65236fecc\"," +
+                                        "--->--->--->\"name\":\"GraphQL in Action\"," +
                                         "--->--->--->\"edition\":3," +
-                                        "--->--->--->\"price\":88.00," +
+                                        "--->--->--->\"price\":80.00," +
                                         "--->--->--->\"store\":{" +
-                                        "--->--->--->--->\"id\":\"d38c10da-6be8-4924-b9b9-5e81899612a0\"," +
-                                        "--->--->--->--->\"name\":\"O'REILLY\"," +
-                                        "--->--->--->--->\"website\":null," +
-                                        "--->--->--->--->\"version\":0" +
+                                        "--->--->--->--->\"id\":\"2fa3955e-3e83-49b9-902e-0465c109c779\"," +
+                                        "--->--->--->--->\"name\":\"MANNING\"" +
                                         "--->--->--->}" +
                                         "--->--->},{" +
                                         "--->--->--->\"id\":\"e110c564-23cc-4811-9e81-d587a13db634\"," +
@@ -336,13 +335,13 @@ public class JoinFetchTest extends AbstractQueryTest {
                                         "--->--->--->\"price\":50.00," +
                                         "--->--->--->\"store\":{" +
                                         "--->--->--->--->\"id\":\"d38c10da-6be8-4924-b9b9-5e81899612a0\"," +
-                                        "--->--->--->--->\"name\":\"O'REILLY\"," +
-                                        "--->--->--->--->\"website\":null,\"version\":0" +
+                                        "--->--->--->--->\"name\":\"O'REILLY\"" +
                                         "--->--->--->}" +
                                         "--->--->}" +
                                         "--->]," +
-                                        "--->\"totalRowCount\":9,\"totalPageCount\":5}" +
-                                        "]"
+                                        "--->\"totalRowCount\":6," +
+                                        "--->\"totalPageCount\":3" +
+                                        "}"
                         );
                     }
             );
