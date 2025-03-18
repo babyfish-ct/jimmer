569948687be4c583ecebc07a7b27d8a77fec9de1
babyfish-ct <babyfish.ct@gmail.com>
2025-03-19 05:17:57 +0800

    Better Kotlin API

--- project/jimmer-sql-kotlin/src/main/kotlin/org/babyfish/jimmer/sql/kt/ast/query/KFilterable.kt
+++ project/jimmer-sql-kotlin/src/main/kotlin/org/babyfish/jimmer/sql/kt/ast/query/KFilterable.kt
@@ -1,5 +1,7 @@
 package org.babyfish.jimmer.sql.kt.ast.query
 
+import org.babyfish.jimmer.kt.DslScope
 import org.babyfish.jimmer.sql.kt.ast.table.KNonNullTable
 
+@DslScope
 interface KFilterable<E: Any> : AbstractKFilterable<E, KNonNullTable<E>>
--- project/jimmer-sql-kotlin/src/main/kotlin/org/babyfish/jimmer/sql/kt/ast/query/KSortable.kt
+++ project/jimmer-sql-kotlin/src/main/kotlin/org/babyfish/jimmer/sql/kt/ast/query/KSortable.kt
@@ -1,5 +1,7 @@
 package org.babyfish.jimmer.sql.kt.ast.query
 
+import org.babyfish.jimmer.kt.DslScope
 import org.babyfish.jimmer.sql.kt.ast.table.KNonNullTable
 
+@DslScope
 interface KSortable<E: Any> : KFilterable<E>, AbstractKSortable<E, KNonNullTable<E>>
