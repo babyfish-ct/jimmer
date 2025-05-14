e7832bb4e8c7e3c6e7813e407b16d4fd1654fcb8
babyfish-ct <babyfish.ct@gmail.com>
2025-05-14 21:27:55 +0800

    #1041

--- project/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/ast/mutation/MutableUpdate.java
+++ project/jimmer-sql/src/main/java/org/babyfish/jimmer/sql/ast/mutation/MutableUpdate.java
@@ -7,6 +7,8 @@ import org.babyfish.jimmer.sql.ast.Predicate;
 import org.babyfish.jimmer.sql.ast.PropExpression;
 import org.babyfish.jimmer.sql.ast.query.Filterable;
 
+import java.util.function.Supplier;
+
 public interface MutableUpdate extends Filterable, Executable<Integer> {
 
     @OldChain
@@ -16,5 +18,25 @@ public interface MutableUpdate extends Filterable, Executable<Integer> {
     <X> MutableUpdate set(PropExpression<X> path, Expression<X> value);
 
     @OldChain
+    @Override
     MutableUpdate where(Predicate ... predicates);
+
+    @OldChain
+    @Deprecated
+    @Override
+    default MutableUpdate whereIf(boolean condition, Predicate predicate) {
+        if (condition) {
+            where(predicate);
+        }
+        return this;
+    }
+
+    @OldChain
+    @Override
+    default MutableUpdate whereIf(boolean condition, Supplier<Predicate> block) {
+        if (condition) {
+            where(block.get());
+        }
+        return this;
+    }
 }
