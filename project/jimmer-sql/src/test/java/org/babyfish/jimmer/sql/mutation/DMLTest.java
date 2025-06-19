dc8a68d8959b08d6606aea04f9dfd38a472ebe28
babyfish-ct <babyfish.ct@gmail.com>
2025-06-19 22:03:59 +0800

    #1086

--- project/jimmer-sql/src/test/java/org/babyfish/jimmer/sql/mutation/DMLTest.java
+++ project/jimmer-sql/src/test/java/org/babyfish/jimmer/sql/mutation/DMLTest.java
@@ -13,6 +13,7 @@ import org.babyfish.jimmer.sql.model.*;
 import org.babyfish.jimmer.sql.model.inheritance.AdministratorMetadataTable;
 import org.babyfish.jimmer.sql.model.inheritance.AdministratorTable;
 import org.babyfish.jimmer.sql.exception.ExecutionException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class DMLTest extends AbstractMutationTest {
@@ -288,10 +289,13 @@ public class DMLTest extends AbstractMutationTest {
     @Test
     public void testIssue1086() {
         BookTable table = BookTable.$;
-        getSqlClient()
-                .createUpdate(table)
-                .set(table.name(), "Unknown Name")
-                .set(table.name(), "Unknown Name")
-                .where(table.id().eq(graphQLInActionId1));
+        IllegalStateException ex = Assertions.assertThrows(IllegalStateException.class, () -> {
+            getSqlClient()
+                    .createUpdate(table)
+                    .set(table.name(), "Unknown Name")
+                    .set(table.name(), "Unknown Name")
+                    .where(table.id().eq(graphQLInActionId1));
+        });
+        Assertions.assertEquals("Cannot update same column twice", ex.getMessage());
     }
 }
