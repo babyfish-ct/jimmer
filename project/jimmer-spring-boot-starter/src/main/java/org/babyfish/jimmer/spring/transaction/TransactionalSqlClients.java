0ab2c60daf9aaa490ae24cea95a46fbef56e09e7
babyfish-ct <babyfish.ct@gmail.com>
2025-06-19 23:40:02 +0800

    #1078

--- project/jimmer-spring-boot-starter/src/main/java/org/babyfish/jimmer/spring/transaction/TransactionalSqlClients.java
+++ project/jimmer-spring-boot-starter/src/main/java/org/babyfish/jimmer/spring/transaction/TransactionalSqlClients.java
@@ -2,10 +2,6 @@ package org.babyfish.jimmer.spring.transaction;
 
 import org.babyfish.jimmer.sql.JSqlClient;
 import org.babyfish.jimmer.sql.kt.KSqlClient;
-import org.babyfish.jimmer.sql.kt.KSqlClientKt;
-import org.babyfish.jimmer.sql.kt.di.AbstractKSqlClientDelegate;
-import org.babyfish.jimmer.sql.kt.impl.KSqlClientImplementor;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * This class should only be used if multiple data sources are used.
@@ -27,18 +23,6 @@ public class TransactionalSqlClients {
 
     private static class KotlinInstanceHolder {
 
-        static final KSqlClient INSTANCE = new AbstractKSqlClientDelegate() {
-
-            private KSqlClientImplementor sqlClient;
-
-            @NotNull
-            protected KSqlClientImplementor sqlClient() {
-                KSqlClientImplementor sqlClient = this.sqlClient;
-                if (sqlClient == null) {
-                    this.sqlClient = sqlClient = (KSqlClientImplementor) KSqlClientKt.toKSqlClient(JAVA_INSTANCE);
-                }
-                return sqlClient;
-            }
-        };
+        static final KSqlClient INSTANCE = new KTransactionalSqlClient();
     }
 }
