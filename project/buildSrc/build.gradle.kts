e16e1a7be04eaa42e2129d1568b931be23a3ca4b
babyfish-ct <babyfish.ct@gmail.com>
2025-10-22 21:06:10 +0800

    upgrade dokka

--- project/buildSrc/build.gradle.kts
+++ project/buildSrc/build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
 
 dependencies {
     api("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.20")
-    api("org.jetbrains.dokka:dokka-gradle-plugin:2.0.0")
+    api("org.jetbrains.dokka:dokka-gradle-plugin:2.1.0")
     api("com.vanniktech:gradle-maven-publish-plugin:0.33.0")
 }
