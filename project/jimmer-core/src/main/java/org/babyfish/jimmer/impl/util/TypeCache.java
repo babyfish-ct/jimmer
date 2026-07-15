cf1e0fe5afbd076104bdd9533edce4ab984df148
belovaf <belovaf@gmail.com>
2026-07-15 17:44:15 +0300

    Use ClassValue for type caches

--- project/jimmer-core/src/main/java/org/babyfish/jimmer/impl/util/TypeCache.java
+++ project/jimmer-core/src/main/java/org/babyfish/jimmer/impl/util/TypeCache.java
@@ -1,31 +1,18 @@
 package org.babyfish.jimmer.impl.util;
 
-import org.babyfish.jimmer.impl.org.objectweb.asm.Type;
 import org.babyfish.jimmer.meta.ImmutableType;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.function.Function;
 
-/*
- * Fight with spring-dev-tools
- */
 public class TypeCache<V> {
 
     private final Function<ImmutableType, V> creator;
 
-    private final ReadWriteLock cacheLock = new ReentrantReadWriteLock();
+    private final boolean nullable;
 
-    private final Map<ImmutableType, V> positiveMap = new HashMap<>();
+    private final ClassCache<Entry<V>> classCache;
 
-    private final Map<String, V> positiveMap2 = new HashMap<>();
-
-    private final Map<ImmutableType, Void> negativeMap;
-
-    private final Map<String, Void> negativeMap2;
+    private final StaticCache<ImmutableType, V> fallbackCache;
 
     public TypeCache(Function<ImmutableType, V> creator) {
         this(creator, false);
@@ -33,69 +20,42 @@ public class TypeCache<V> {
 
     public TypeCache(Function<ImmutableType, V> creator, boolean nullable) {
         this.creator = creator;
-        negativeMap = nullable ? new LRUMap<>() : null;
-        negativeMap2 = nullable ? new LRUMap<>() : null;
+        this.nullable = nullable;
+        classCache = new ClassCache<>(this::createEntry);
+        fallbackCache = new StaticCache<>(creator, nullable);
     }
 
     public V get(ImmutableType key) {
-
-        String keyString = key != null ? key.toString() : null;
-        V value;
-        Lock lock;
-
-        (lock = cacheLock.readLock()).lock();
-        try {
-            if (negativeMap != null && negativeMap.containsKey(key)) {
-                return null;
-            }
-            if (negativeMap2 != null && negativeMap2.containsKey(keyString)) {
-                negativeMap.put(key, null);
-            }
-            value = positiveMap.get(key);
-            if (value == null) {
-                value = positiveMap2.get(keyString);
-                if (value != null) {
-                    positiveMap.put(key, value);
-                }
-            }
-        } finally {
-            lock.unlock();
+        Entry<V> entry = classCache.get(key.getJavaClass());
+        if (entry.type == key) {
+            return entry.value;
         }
+        return fallbackCache.get(key);
+    }
 
-        if (value == null) {
-            (lock = cacheLock.writeLock()).lock();
-            try {
-                if (negativeMap != null && negativeMap.containsKey(key)) {
-                    return null;
-                }
-                if (negativeMap2 != null && negativeMap2.containsKey(keyString)) {
-                    negativeMap.put(key, null);
-                }
-                value = positiveMap.get(key);
-                if (value == null) {
-                    value = positiveMap2.get(keyString);
-                    if (value != null) {
-                        positiveMap.put(key, value);
-                    } else {
-                        value = creator.apply(key);
-                        if (value != null) {
-                            positiveMap.put(key, value);
-                            positiveMap2.put(keyString, value);
-                        } else if (negativeMap != null) {
-                            negativeMap.put(key, null);
-                            negativeMap2.put(keyString, null);
-                        } else {
-                            throw new IllegalStateException(
-                                    "The creator cannot return null because current type cache does not accept null values"
-                            );
-                        }
-                    }
-                }
-            } finally {
-                lock.unlock();
-            }
+    private Entry<V> createEntry(Class<?> javaClass) {
+        ImmutableType type = ImmutableType.tryGet(javaClass);
+        if (type == null) {
+            return new Entry<>(null, null);
         }
+        V value = creator.apply(type);
+        if (value == null && !nullable) {
+            throw new IllegalStateException(
+                    "The creator cannot return null because current type cache does not accept null values"
+            );
+        }
+        return new Entry<>(type, value);
+    }
 
-        return value;
+    private static class Entry<V> {
+
+        final ImmutableType type;
+
+        final V value;
+
+        private Entry(ImmutableType type, V value) {
+            this.type = type;
+            this.value = value;
+        }
     }
 }
