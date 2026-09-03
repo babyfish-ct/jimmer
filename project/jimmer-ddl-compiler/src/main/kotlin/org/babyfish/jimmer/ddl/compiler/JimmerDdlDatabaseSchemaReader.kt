7b6c18361aa72295573aa3cc7d384d4abcf2c060
babyfish-ct <babyfish.ct@gmail.com>
2026-09-03 22:57:15 +0800

    Fix compilation error

--- project/jimmer-ddl-compiler/src/main/kotlin/org/babyfish/jimmer/ddl/compiler/JimmerDdlDatabaseSchemaReader.kt
+++ project/jimmer-ddl-compiler/src/main/kotlin/org/babyfish/jimmer/ddl/compiler/JimmerDdlDatabaseSchemaReader.kt
@@ -46,20 +46,21 @@ object JimmerDdlDatabaseSchemaReader {
             Class.forName(driverClassName)
         }
 
-        DriverManager.getConnection(jdbc.url, settings.jdbcConnectionProperties()).use { connection ->
-        val properties = Properties().apply {
-            if (jdbc.username.isNotBlank()) {
-                setProperty("user", jdbc.username)
+        return DriverManager.getConnection(jdbc.url, settings.jdbcConnectionProperties()).use { connection ->
+            val properties = Properties().apply {
+                if (jdbc.username.isNotBlank()) {
+                    setProperty("user", jdbc.username)
+                }
+                if (jdbc.password.isNotBlank()) {
+                    setProperty("password", jdbc.password)
+                }
+                setProperty("connectTimeout", "5000")
+                setProperty("loginTimeout", "5")
             }
-            if (jdbc.password.isNotBlank()) {
-                setProperty("password", jdbc.password)
+            DriverManager.getConnection(jdbc.url, properties).use { connection ->
+                val schemaName = jdbc.schema ?: runCatching { connection.schema }.getOrNull()
+                return readSchema(connection, schemaName, desiredSchema, renamedTables)
             }
-            setProperty("connectTimeout", "5000")
-            setProperty("loginTimeout", "5")
-        }
-        DriverManager.getConnection(jdbc.url, properties).use { connection ->
-            val schemaName = jdbc.schema ?: runCatching { connection.schema }.getOrNull()
-            return readSchema(connection, schemaName, desiredSchema, renamedTables)
         }
     }
 
